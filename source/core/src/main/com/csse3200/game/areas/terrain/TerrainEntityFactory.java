0d7decff0e1b8079e7ee0dc0fa90dddca987845e
Lucas Hicks <lucasjhicks1@gmail.com>
2023-08-15 23:52:57 +1000

    Adds abstract class TerrainEntityFactory skeleton #16

---
+++ source/core/src/main/com/csse3200/game/areas/terrain/TerrainEntityFactory.java
@@ -0,0 +1,8 @@
+package com.csse3200.game.areas.terrain;
+
+import com.csse3200.game.entities.Entity;
+
+public abstract class TerrainEntityFactory {
+
+  public abstract Entity CreateTerrainEntity();
+}
