80673043d7561c657ecde35365054bbfb8ee94e3
Lucas Hicks <lucasjhicks1@gmail.com>
2023-08-16 00:02:02 +1000

    Adds javadocs to abstract TerrainEntityFactory class #16

--- source/core/src/main/com/csse3200/game/areas/terrain/TerrainEntityFactory.java
+++ source/core/src/main/com/csse3200/game/areas/terrain/TerrainEntityFactory.java
@@ -2,7 +2,15 @@ package com.csse3200.game.areas.terrain;
 
 import com.csse3200.game.entities.Entity;
 
+
+/**
+ * Abstract factory that is inherited by factories that create Terrain Entities
+ */
 public abstract class TerrainEntityFactory {
 
+  /**
+   * Creates terrain entity
+   * @return terrain entity
+   */
   public abstract Entity CreateTerrainEntity();
 }
