1fc76866aabb3e6cda41a17d11efe59c44bc2cb1
86LAK <lakshan392@gmail.com>
2023-08-16 11:23:35 +1000

    Item Class #15 adding price, sellable and commenting

--- source/core/src/main/com/csse3200/game/components/player/Item.java
+++ source/core/src/main/com/csse3200/game/components/player/Item.java
@@ -13,6 +13,8 @@ public class Item extends Component {
     private String itemDescription; // User facing description for item.
     private final String itemId;
     private final ItemType itemType; 
+    private int price; // Price of item
+    private final boolean sellable; // is the item sellable
 
     public enum ItemType {
         TOOL,
@@ -31,6 +33,23 @@ public class Item extends Component {
         this.itemId = generateUniqueID(); // Generate a unique ID for the item
         this.itemType = itemType; //from enum ItemType
         this.itemDescription = "" ; //default description
+        this.price = 0;
+        this.sellable = false; //default sellable 
+    }
+
+    /**
+     * Constructor for Item
+     * @param itemName user facing name 
+     * @param itemType type of the item
+     * @param price sellable price of the item
+     */
+    public Item(String itemName, ItemType itemType, int price) {
+        this.itemName = itemName; 
+        this.itemId = generateUniqueID(); // Generate a unique ID for the item
+        this.itemType = itemType; //from enum ItemType
+        this.itemDescription = "" ; //default description
+        this.price = price;
+        this.sellable = true;
     }
 
     /**
@@ -44,24 +63,80 @@ public class Item extends Component {
         this.itemId = generateUniqueID(); // Generate a unique ID for the item
         this.itemType = itemType; //from enum ItemType
         this.itemDescription = itemDescription ; //default description
+        this.sellable = false; //default sellable
     }
 
+    /**
+     * Constructor for Item
+     * @param itemName user facing name for item
+     * @param itemType type of item 
+     * @param itemDescription user facing description for item
+     * @param price price of item
+     */
+    public Item(String itemName, ItemType itemType, String itemDescription, int price) {
+        this.itemName = itemName; 
+        this.itemId = generateUniqueID(); // Generate a unique ID for the item
+        this.itemType = itemType; //from enum ItemType
+        this.itemDescription = itemDescription ; //default description
+        this.price = price;
+        this.sellable = true;
+    }
+
+
+    /** 
+     * Returns the price of the item
+     * @return int price of item
+     */
+    public int getPrice() {
+        return price;
+    }
+
+
+    /**
+     * Returns selalble bool of item
+     * @return true if item is sellable, false otherwise
+     */
+    public boolean isSellable() {
+        return sellable;
+    }
+
+    /**
+     * Returns the type of the item
+     * @return ItemType type of item
+     */
     public ItemType getItemType() {
         return itemType;
     }
 
+    
+    /**
+     * Returns the name of the item
+     * @return int price of item
+     */
     public String getItemName() {
         return itemName;
     }
 
+    /**
+     * Returns the id of the item
+     * @return String id of item
+     */
     public String getItemId() {
         return itemId;
     }
 
+    /**
+     * Returns the description of the item
+     * @return String description of item
+     */
     public String getItemDescription() {
         return itemDescription;
     }
 
+    /**
+     * Sets the item name of the item
+     * @param String name of the item
+     */
     public void setItemName(String name) {
         itemName = name;
     }
