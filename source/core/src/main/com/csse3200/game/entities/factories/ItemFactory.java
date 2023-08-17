8ac4544954597fc2f4d014d1085be7993248fa8b
86LAK <lakshan392@gmail.com>
2023-08-17 19:41:08 +1000

    merge fixes Item Class #15

--- source/core/src/main/com/csse3200/game/entities/factories/ItemFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/ItemFactory.java
@@ -10,14 +10,35 @@ import com.csse3200.game.rendering.TextureRenderComponent;
 /**
  * Factory to create an item
  */
+//TODO change to hoe and shovel. fix base item to not take a texture path
 public class ItemFactory {
     public static Entity createBaseItem() {
         Entity item = new Entity()
-                .addComponent(new TextureRenderComponent("images/box_boy_leaf.png"))
                 .addComponent(new PhysicsComponent())
                 .addComponent(new ColliderComponent())
                 .addComponent(new HitboxComponent().setLayer(PhysicsLayer.DEFAULT))
-                .addComponent(new ItemComponent("shovel", "images/tool_shovel.png"));
+                .addComponent(new ItemComponent("hoe", "images/tool_hoe.png"));
         return item;
     }
+
+    public static Entity createShovel() {
+      Entity testTool = createBaseItem()
+              .addComponent(new TextureRenderComponent("images/tool_shovel.png"));
+      //testTool.getComponent(TextureRenderComponent.class).scaleEntity();  // dont know what this does
+      testTool.scaleHeight(1f); // scales the Entity!, this could be scaleWidth too
+      return testTool;
+  }
+
+      // Hoe tool
+      public static Entity createHoe() {
+        Entity hoe = createBaseItem()
+                .addComponent(new TextureRenderComponent("images/tool_hoe.png"));
+        // do some more stuff here...
+        /* add more components to build on baseTool:
+
+         */
+        return hoe;
+    }
+
+
 }
