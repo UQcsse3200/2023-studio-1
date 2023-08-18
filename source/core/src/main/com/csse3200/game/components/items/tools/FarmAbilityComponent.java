e2007d280480ff094c672ca7ed2e31941dd589a9
cmp6 <s4650089@student.uq.edu.au>
2023-08-18 17:27:34 +1000

    fixed FarmAbilityComponent.getAbility to return an ability and take no arguments

--- source/core/src/main/com/csse3200/game/components/items/tools/FarmAbilityComponent.java
+++ source/core/src/main/com/csse3200/game/components/items/tools/FarmAbilityComponent.java
@@ -2,7 +2,7 @@ package com.csse3200.game.components.items.tools;
 
 import com.csse3200.game.components.Component;
 
-/*
+/**
  * provides selection of farming properties to be added to entities, such as:
  *  tilling, digging, watering.
  */
@@ -10,20 +10,26 @@ public class FarmAbilityComponent extends Component {
 
   private FarmAbilities ability;
 
-  /* Constructor
+  /**
+   * Constructor
    * Takes and sets desired ability for that instance.
    */
   public FarmAbilityComponent(FarmAbilities ability) {
     this.ability = ability;
   }
 
-  /*
+  /**
    * Modifies ability, useful for individual tools
    */
   public void setAbility(FarmAbilities ability) {
      this.ability = ability;
   }
-  public FarmAbilities getAbility(FarmAbilities ability) {
+
+  /**
+   *
+   * @return ability attribute
+   */
+  public FarmAbilities getAbility() {
     return this.ability;
   }
 }
