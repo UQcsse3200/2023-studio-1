4e6cd83a0ed22e1dd03cf91c9e3c6d5d3c2406b1
Lucas Hicks <lucasjhicks1@gmail.com>
2023-08-18 12:02:22 +1000

    Scales collider of crop tile entity to 100% of size, both vertically and horizontally #18

--- source/core/src/main/com/csse3200/game/areas/terrain/TerrainCropTileFactory.java
+++ source/core/src/main/com/csse3200/game/areas/terrain/TerrainCropTileFactory.java
@@ -1,6 +1,7 @@
 package com.csse3200.game.areas.terrain;
 
 import com.csse3200.game.entities.Entity;
+import com.csse3200.game.physics.PhysicsUtils;
 import com.csse3200.game.physics.components.ColliderComponent;
 import com.csse3200.game.rendering.TextureRenderComponent;
 
@@ -18,6 +19,13 @@ public class TerrainCropTileFactory implements TerrainEntityFactory {
     Entity tile = new Entity()
         .addComponent(new ColliderComponent())
         .addComponent(new TextureRenderComponent("images/tool_shovel.png"));
+
+    // Implement crop tile component
+    // Change sprite used in texture
+    // Load in tile config
+
+    PhysicsUtils.setScaledCollider(tile, 1.0f, 1.0f);
+
     return tile;
   }
 }
