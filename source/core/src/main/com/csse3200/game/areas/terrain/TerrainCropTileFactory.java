51cad666b77d3b26dc4e8d7663a895b64bb16df4
Lucas Hicks <lucasjhicks1@gmail.com>
2023-08-18 11:16:34 +1000

    Implements TerrainEntityFactory interface in TerrainCropTileFactory #18

---
+++ source/core/src/main/com/csse3200/game/areas/terrain/TerrainCropTileFactory.java
@@ -0,0 +1,19 @@
+package com.csse3200.game.areas.terrain;
+
+import com.csse3200.game.entities.Entity;
+
+public class TerrainCropTileFactory implements
+    TerrainEntityFactory {
+
+  /**
+   * Creates a terrain entity
+   *
+   * @param x x-position of the entity to be created
+   * @param y y-position of the entity to be created
+   * @return created entity
+   */
+  @Override
+  public Entity CreateTerrainEntity(int x, int y) {
+    return null;
+  }
+}
