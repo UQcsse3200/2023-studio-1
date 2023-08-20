76d1ce124bfb2e4af6bd736df70253406d7d41f5
Lucas Hicks <lucasjhicks1@gmail.com>
2023-08-20 18:15:40 +1000

    Implements test methods in TerrainCropTileFactory test, cropTileComponentDetected() and cropTilePositionCorrect(). (See issue #18)"

--- source/core/src/test/com/csse3200/game/areas/terrain/TerrainCropTileFactoryTest.java
+++ source/core/src/test/com/csse3200/game/areas/terrain/TerrainCropTileFactoryTest.java
@@ -1,12 +1,48 @@
 package com.csse3200.game.areas.terrain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import com.badlogic.gdx.math.Vector2;
 import com.csse3200.game.entities.Entity;
+import com.csse3200.game.entities.EntityService;
+import com.csse3200.game.extensions.GameExtension;
+import com.csse3200.game.physics.PhysicsService;
+import com.csse3200.game.rendering.RenderService;
+import com.csse3200.game.services.ResourceService;
+import com.csse3200.game.services.ServiceLocator;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(GameExtension.class)
 class TerrainCropTileFactoryTest {
-
   private static Entity tile;
 
+  @BeforeEach
+  void BeforeEach() {
+    ServiceLocator.clear();
+    ServiceLocator.registerResourceService(mock(ResourceService.class));
+    ServiceLocator.registerEntityService(new EntityService());
+    ServiceLocator.registerPhysicsService(new PhysicsService());
+    ServiceLocator.registerEntityService(new EntityService());
+    ServiceLocator.registerRenderService(new RenderService());
+  }
 
+  @Test
+  public void cropTileComponentDetected() {
+    tile = TerrainCropTileFactory.CreateTerrainEntity(0,0);
+    CropTileComponent cropTileComponent = tile.getComponent(CropTileComponent.class);
+    assertNotNull(cropTileComponent);
+  }
+
+  @Test
+  public void cropTilePositionCorrect() {
+    Vector2 position = new Vector2(0,0);
+    tile = TerrainCropTileFactory.CreateTerrainEntity(position);
+    assertEquals(position, tile.getPosition());
+  }
 }
