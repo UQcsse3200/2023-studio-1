d2aeda8e4cc855969f2a380ced518d8353303d4f
Lucas Hicks <lucasjhicks1@gmail.com>
2023-08-20 17:13:28 +1000

    Adds TerrainCropTileFactory test file skeleton #18

---
+++ source/core/src/test/com/csse3200/game/areas/terrain/TerrainCropTileFactoryTest.java
@@ -0,0 +1,12 @@
+package com.csse3200.game.areas.terrain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.csse3200.game.entities.Entity;
+
+class TerrainCropTileFactoryTest {
+
+  private static Entity tile;
+
+
+}
