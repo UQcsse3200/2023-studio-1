9d0ccc3bb9f93f8017a9c260348ab2c0c823d4e8
Lucas Hicks <lucasjhicks1@gmail.com>
2023-08-20 16:59:49 +1000

    Adds logger to TerrainCropTileFactory. Implements newly created CropTileComponent (#19) into entity creation. Also registers entity with entity service for event handling. Also changes ColliderComponent of entity to be a sensor ensuring that the crop tile does not collide with other objects. (See issue #18)

--- source/core/src/main/com/csse3200/game/areas/terrain/TerrainCropTileFactory.java
+++ source/core/src/main/com/csse3200/game/areas/terrain/TerrainCropTileFactory.java
@@ -1,14 +1,24 @@
 package com.csse3200.game.areas.terrain;
 
 import com.csse3200.game.entities.Entity;
+import com.csse3200.game.entities.configs.CropTileConfig;
+import com.csse3200.game.files.FileLoader;
 import com.csse3200.game.physics.PhysicsUtils;
 import com.csse3200.game.physics.components.ColliderComponent;
 import com.csse3200.game.rendering.TextureRenderComponent;
+import com.csse3200.game.services.ServiceLocator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TerrainCropTileFactory implements TerrainEntityFactory {
 
+  private static final Logger logger = LoggerFactory.getLogger(TerrainCropTileFactory.class);
+
+  private static final CropTileConfig stats =
+      FileLoader.readClass(CropTileConfig.class, "configs/cropTile.json");
+
   /**
-   * Creates a terrain entity
+   * Creates a crop tile entity
    *
    * @param x x-position of the entity to be created
    * @param y y-position of the entity to be created
@@ -16,17 +26,22 @@ public class TerrainCropTileFactory implements TerrainEntityFactory {
    */
   @Override
   public Entity CreateTerrainEntity(float x, float y) {
+    logger.debug("Creating crop tile at position ({},{})", x, y);
+
     Entity tile = new Entity()
-        .addComponent(new ColliderComponent())
-        .addComponent(new TextureRenderComponent("images/tool_shovel.png"));
+        // TODO Change Sprite used in RenderComponent - waiting on #27
+        .addComponent(new TextureRenderComponent("images/tool_shovel.png"))
+        .addComponent(new CropTileComponent(1,1));
+        // TODO Set CropTileComponent params to values in  CropTileConfig class
 
-
-    // Implement crop tile component
-    // Change sprite used in texture
-    // Load in tile config
+    // Adds ColliderComponent as sensor and not physics collider
+    ColliderComponent sensor = new ColliderComponent();
+    sensor.setSensor(true);
+    tile.addComponent(sensor);
 
     tile.setPosition(x, y);
-    PhysicsUtils.setScaledCollider(tile, 1.0f, 1.0f);
+    logger.debug("Registering crop tile {} with entity service", tile);
+    ServiceLocator.getEntityService().register(tile);
 
     return tile;
   }
