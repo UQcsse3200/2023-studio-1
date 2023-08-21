17991a597c02d8707b36cbb26b8a307b21a1a41e
DonghyugDavidJeong <donghyug.jeong@uqconnect.edu.au>
2023-08-21 11:34:57 +1000

    #19 Added 5 different types of cropTiles in test to test various conditions. Added tests for the "water" event.

--- source/core/src/test/com/csse3200/game/areas/terrain/CropTileComponentTest.java
+++ source/core/src/test/com/csse3200/game/areas/terrain/CropTileComponentTest.java
@@ -15,19 +15,64 @@ import com.csse3200.game.extensions.GameExtension;
 import com.csse3200.game.physics.components.ColliderComponent;
 import com.csse3200.game.physics.components.HitboxComponent;
 import com.csse3200.game.physics.components.PhysicsComponent;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(GameExtension.class)
 public class CropTileComponentTest {
+    private Entity cropTile1, cropTile2, cropTile3, cropTile4, cropTile5, cropTile6;
+    @BeforeEach
+    public void init() {
+        cropTile1 = new Entity().addComponent(new CropTileComponent(1f, 0.5f));
+        cropTile2 = new Entity().addComponent(new CropTileComponent(0.5f, 0.5f));
+        cropTile3 = new Entity().addComponent(new CropTileComponent(1f, 1.0f));
+        cropTile4 = new Entity().addComponent(new CropTileComponent(0f, 1.0f));
+        cropTile5 = new Entity().addComponent(new CropTileComponent(1f, 0.0f));
+        cropTile1.create();
+        cropTile2.create();
+        cropTile3.create();
+        cropTile4.create();
+        cropTile5.create();
+    }
 
     @Test
-    void shouldGiveExpectedGrowthRate() {
-        Entity cropTile1 =
-                new Entity()
-                        .addComponent(new CropTileComponent(1f, 0.5f));
+    public void shouldGiveExpectedGrowthRate() {
         assertEquals(0.5, cropTile1.getComponent(CropTileComponent.class).getGrowthRate(), 0.00001);
+        assertEquals(0.27694, cropTile2.getComponent(CropTileComponent.class).getGrowthRate(), 0.00001);
+        assertEquals(1.0, cropTile3.getComponent(CropTileComponent.class).getGrowthRate(), 0.00001);
+        assertEquals(-1.0, cropTile4.getComponent(CropTileComponent.class).getGrowthRate(), 0.0f);
+        assertEquals(0, cropTile5.getComponent(CropTileComponent.class).getGrowthRate(), 0.0f);
+    }
 
+    @Test
+    public void shouldGiveWateredGrowthRate() {
+        cropTile1.getEvents().trigger("water", 0.5f);
+        cropTile2.getEvents().trigger("water", 0.5f);
+        cropTile3.getEvents().trigger("water", 0.5f);
+        cropTile4.getEvents().trigger("water", 0.5f);
+        cropTile5.getEvents().trigger("water", 0.5f);
+        assertEquals(0.27694, cropTile1.getComponent(CropTileComponent.class).getGrowthRate(), 0.00001);
+        assertEquals(0.5, cropTile2.getComponent(CropTileComponent.class).getGrowthRate(), 0.00001);
+        assertEquals(0.55387, cropTile3.getComponent(CropTileComponent.class).getGrowthRate(), 0.00001);
+        assertEquals(0.55387, cropTile4.getComponent(CropTileComponent.class).getGrowthRate(), 0.00001);
+        assertEquals(0, cropTile5.getComponent(CropTileComponent.class).getGrowthRate(), 0.0f);
+        cropTile4.getEvents().trigger("water", 0.5f);
+        assertEquals(1.0, cropTile4.getComponent(CropTileComponent.class).getGrowthRate(), 0.00001);
+        cropTile4.getEvents().trigger("water", 0.5f);
+        assertEquals(0.55387, cropTile4.getComponent(CropTileComponent.class).getGrowthRate(), 0.00001);
+    }
+
+    @Test
+    public void shouldGiveFertilisedGrowthRate() {
+        cropTile1.getEvents().trigger("fertilise");
+        cropTile2.getEvents().trigger("fertilise");
+        cropTile3.getEvents().trigger("fertilise");
+        cropTile4.getEvents().trigger("fertilise");
+        cropTile5.getEvents().trigger("fertilise");
+        assertEquals(1, cropTile1.getComponent(CropTileComponent.class).getGrowthRate(), 0.00001);
+        assertEquals(1, cropTile2.getComponent(CropTileComponent.class).getGrowthRate(), 0.00001);
+        assertEquals(0, cropTile5.getComponent(CropTileComponent.class).getGrowthRate(), 0.0f);
     }
 
 }
