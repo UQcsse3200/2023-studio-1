1f685e6851baca403b05d691c13ecb32942f2a9b
Sanchit Jain <sanchitjain1632@gmail.com>
2023-08-23 13:03:12 +1000

    Added "Nicotiana Tabacum" for the factory of type Deadly

--- source/core/src/main/com/csse3200/game/entities/configs/plants/PlantConfigs.java
+++ source/core/src/main/com/csse3200/game/entities/configs/plants/PlantConfigs.java
@@ -8,5 +8,5 @@ public class PlantConfigs {
     public HealthPlantConfig aloeVera = new HealthPlantConfig();
     public RepairPlantConfig hammerPlant = new RepairPlantConfig();
     public DeadlyPlantConfig atropaBelladonna = new DeadlyPlantConfig();
-
+    public DeadlyPlantConfig nicotianaTabacum = new DeadlyPlantConfig();
 }
