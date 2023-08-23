ab07ef4e008a08d89a8cd28cf21ae41282c38fde
Shristi Gupta <guptashristi1408@gmail.com>
2023-08-23 11:15:24 +1000

    Added CropTileConfig #28

--- source/core/src/main/com/csse3200/game/entities/configs/CropTileConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/CropTileConfig.java
@@ -2,4 +2,7 @@ package com.csse3200.game.entities.configs;
 
 public class CropTileConfig extends BaseEntityConfig {
 
+    public int initialSoilQuality = 1;  //Default value for initial soil quality
+    public int initialWaterContent = 1; //Default value for initial water content
+
 }
