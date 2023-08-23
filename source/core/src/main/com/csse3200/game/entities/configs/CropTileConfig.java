5e255825f64054187c34525973149d40fd2cc8d2
Shristi Gupta <guptashristi1408@gmail.com>
2023-08-23 11:26:02 +1000

    Added JavaDoc comment to CropTileConfig #28

--- source/core/src/main/com/csse3200/game/entities/configs/CropTileConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/CropTileConfig.java
@@ -1,5 +1,8 @@
 package com.csse3200.game.entities.configs;
 
+/**
+ * Defines the configuration for a crop tile entity.
+ */
 public class CropTileConfig extends BaseEntityConfig {
 
     public int initialSoilQuality = 1;  //Default value for initial soil quality
