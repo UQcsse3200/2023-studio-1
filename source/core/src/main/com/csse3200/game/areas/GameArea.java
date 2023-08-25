e815b82cf0e9294b4e1433c7c083ecd43807df61
Peter Lahanas <plahanas18@gmail.com>
2023-08-25 16:44:36 +1000

    \#46 Added dispose back to Gdx app

--- source/core/src/main/com/csse3200/game/areas/GameArea.java
+++ source/core/src/main/com/csse3200/game/areas/GameArea.java
@@ -82,6 +82,6 @@ public abstract class GameArea implements Disposable {
   public void removeEntity(Entity entity) {
     entity.setEnabled(false);
     areaEntities.remove(entity);
-    //Gdx.app.postRunnable(entity::dispose); //TODO: What does this do
+    Gdx.app.postRunnable(entity::dispose); //TODO: What does this do
   }
 }
