b89d586b60880805d5ca421f5ce26f87559ebcb3
Manav Trivedi <trivedimanav2003@gmail.com>
2023-08-25 15:58:22 +1000

    Added skeleton for timeObserver class

--- source/core/src/main/com/csse3200/game/services/GameTime.java
+++ source/core/src/main/com/csse3200/game/services/GameTime.java
@@ -45,3 +45,4 @@ public class GameTime {
     return getTime() - lastTime;
   }
 }
+
---
+++ source/core/src/main/com/csse3200/game/services/TimeObserver.java
@@ -0,0 +1,14 @@
+package com.csse3200.game.services;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.TimeUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/** Class that is repsonsible updating classes depedent on the time*/
+public class TimeObserver {
+
+    /** Placeholder for when plants will eventually be added. Plant growth will be dependent on time*/
+    // private List<plant> activePlants = new ArrayList<>
+
+}
