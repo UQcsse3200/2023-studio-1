e8378636953ad210155a2b266810d5f3289ad7b9
Gil Wise <gilwise245@gmail.com>
2023-08-27 21:45:23 +1000

    Revert "Revert "Initialised TimeControllerTest class with blank tests, did not stage file in prior commit""
    This reverts commit e5f99dba8e7cae10a94e49df49b3418f1092413e.

--- source/core/src/test/com/csse3200/game/services/TimeControllerTest.java
+++ source/core/src/test/com/csse3200/game/services/TimeControllerTest.java
@@ -1,4 +1,70 @@
+package com.csse3200.game.services;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 class TimeControllerTest {
-  
+    
+    TimeController timeController;
+    
+    @BeforeAll
+    static void beforeAll() {
+        GameTime mockGameTime = mock(GameTime.class);
+        when(Gdx.graphics.getDeltaTime()).thenReturn(10f);
+    }
+    
+    @BeforeEach
+    void setUp() {
+        timeController = new TimeController();
+    }
+    
+    @AfterEach
+    void tearDown() {
+    }
+    
+    @Test
+    void setTimeSource() {
+    
+    }
+    
+    @Test
+    void setTimeDisplay() {
+    }
+    
+    @Test
+    void getTimeInSeconds() {
+    }
+    
+    @Test
+    void getTimeOfDay() {
+    }
+    
+    @Test
+    void getHour() {
+    }
+    
+    @Test
+    void updateDisplay() {
+    }
+    
+    @Test
+    void pause() {
+    }
+    
+    @Test
+    void unpause() {
+    }
+    
+    @Test
+    void setTime() {
+    }
 }
