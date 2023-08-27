a0b89cdc36166d6c3dc0fbc6cf0f4f58ab105e0f
Gil Wise <gilwise245@gmail.com>
2023-08-27 21:52:14 +1000

    Initialised TimeControllerTest class with blank tests

---
+++ source/core/src/test/com/csse3200/game/services/TimeControllerTest.java
@@ -0,0 +1,65 @@
+package com.csse3200.game.services;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimeControllerTest {
+    
+    TimeController timeController;
+    
+    @BeforeAll
+    static void beforeAll() {
+    }
+    
+    @BeforeEach
+    void setUp() {
+        timeController = new TimeController();
+    }
+    
+    @AfterEach
+    void tearDown() {
+    }
+    
+    @Test
+    void setTimeSource() {
+    
+    }
+    
+    @Test
+    void setTimeDisplay() {
+    }
+    
+    @Test
+    void getTimeInSeconds() {
+    }
+    
+    @Test
+    void getTimeOfDay() {
+    }
+    
+    @Test
+    void getHour() {
+    }
+    
+    @Test
+    void updateDisplay() {
+    }
+    
+    @Test
+    void pause() {
+    }
+    
+    @Test
+    void unpause() {
+    }
+    
+    @Test
+    void setTime() {
+    }
+}
