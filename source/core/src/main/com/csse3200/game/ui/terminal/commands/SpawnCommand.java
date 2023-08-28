70787ae1e8fb2251f6f58fd1797cfed9c223467d
Lucas Hicks <lucasjhicks1@gmail.com>
2023-08-28 17:59:45 +1000

    Adds in the SpawnCommand class which allows players to spawn in animals at the players position or another given position (See issue #93).

--- source/core/src/main/com/csse3200/game/ui/terminal/commands/SpawnCommand.java
+++ source/core/src/main/com/csse3200/game/ui/terminal/commands/SpawnCommand.java
@@ -1,11 +1,13 @@
 package com.csse3200.game.ui.terminal.commands;
 
+import com.badlogic.gdx.math.Vector2;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.entities.factories.NPCFactory;
 import com.csse3200.game.services.ServiceLocator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.Serial;
 import java.util.ArrayList;
 
 public class SpawnCommand implements Command {
@@ -24,11 +26,29 @@ public class SpawnCommand implements Command {
 		}
 		Entity entity;
 		switch (args.get(0)) {
-			case "cow":
-				entity = NPCFactory.createCow();
-				break;
+			case "cow" -> entity = NPCFactory.createCow();
+			case "chicken" -> entity = NPCFactory.createChicken(ServiceLocator.getGameArea().getPlayer());
+			default -> {
+				logger.debug("Entity argument is not valid");
+				return false;
+			}
 		}
-		return false;
+		Vector2 position;
+		if (args.size() == 3) {
+			try {
+				float x = Integer.parseInt(args.get(1));
+				float y = Integer.parseInt(args.get(2));
+				position = new Vector2(x, y);
+			} catch (NumberFormatException e) {
+				logger.debug("Arguments provided were not a valid integer");
+				return false;
+			}
+		} else {
+			position = ServiceLocator.getGameArea().getPlayer().getPosition();
+		}
+		entity.setPosition(position);
+		ServiceLocator.getEntityService().register(entity);
+		return true;
 	}
 
 	boolean correctNumArgs(ArrayList<String> args) {
