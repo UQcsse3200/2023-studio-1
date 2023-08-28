37621e98241bc65de35fee6f2acf29543ec2ae2f
Lucas Hicks <lucasjhicks1@gmail.com>
2023-08-28 22:09:57 +1000

    Adds some basic getters and setters aswell as implement the update() method which is called every time a frame is drawn (See issue #102)."

--- source/core/src/main/com/csse3200/game/services/TimeService.java
+++ source/core/src/main/com/csse3200/game/services/TimeService.java
@@ -1,5 +1,6 @@
 package com.csse3200.game.services;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.utils.Array;
 import com.csse3200.game.entities.Entity;
 import org.slf4j.Logger;
@@ -10,14 +11,31 @@ public class TimeService {
 	private static final int INITIAL_CAPACITY = 16;
 	private static final String HOUR_UPDATE = "hourUpdate";
 	private static final String DAY_UPDATE = "dayUpdate";
+	private static final int MS_IN_HOUR = 30000;
 	private final Array<Entity> hourUpdateEntities = new Array<>(false, INITIAL_CAPACITY);
 	private final Array<Entity> dayUpdateEntities = new Array<>(false, INITIAL_CAPACITY);
-	private static int hour;
-	private static int day;
+	private int hour;
+	private int day;
+	private long timeBuffer;
+	private long lastGameTime;
+	private final GameTime gameTime;
+	private boolean paused;
+
 
 	public TimeService() {
 		hour = 0;
 		day = 0;
+		paused = false;
+		gameTime = new GameTime();
+		lastGameTime = gameTime.getTime();
+	}
+
+	public boolean isPaused() {
+		return paused;
+	}
+
+	public void setPaused(boolean state) {
+		paused = state;
 	}
 
 	public int getHour() {
@@ -26,6 +44,13 @@ public class TimeService {
 
 	public int getDay() {
 		return day;
+	}
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+
+	public void setDay(int day) {
+		this.day = day;
 	}
 
 	public void registerHourUpdate(Entity entity) {
@@ -56,8 +81,28 @@ public class TimeService {
 		}
 	}
 	public void update() {
-		// Some logic to calculate day based on methods in timeSource
-		return;
+		// this time will be in ms
+		long timePassed = gameTime.getTimeSince(lastGameTime);
+		lastGameTime = gameTime.getTime();
+		if (paused) {
+			return;
+		}
+		timeBuffer += timePassed;
+		// If time elapsed isn't one hour in the game, do nothing
+		if (timeBuffer < MS_IN_HOUR) {
+			return;
+		}
+		timeBuffer -= MS_IN_HOUR;
+		hour += 1;
+		triggerHourUpdate();
+
+		// If hour is between 0 and 23, day hasn't elapsed, do nothing
+		if (hour < 24) {
+			return;
+		}
+		hour -= 24;
+		day += 1;
+		triggerDayUpdate();
 	}
 
 }
