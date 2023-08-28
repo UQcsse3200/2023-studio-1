551e3d5bfd54b278d0523954aec2b7745bd24c38
yetanothercarbot <55005345+yetanothercarbot@users.noreply.github.com>
2023-08-28 20:33:13 +1000

    Interaction animation
    Finally, animation.
    
    Remove erroneous println statement.

--- source/core/src/main/com/csse3200/game/components/player/PlayerAnimationController.java
+++ source/core/src/main/com/csse3200/game/components/player/PlayerAnimationController.java
@@ -24,6 +24,32 @@ public class PlayerAnimationController extends Component {
     }
 
     /**
+     * Get the direction the player is facing from the currently playing animation. If
+     * unable to ascertain, it will default to "down"
+     *
+     * @return A string containing either "up", "right", "down" or "left"
+     */
+    String getFacingDir() {
+        String validOutputs = "up right down left";
+        String[] currentAnim = animator.getCurrentAnimation().split("_");
+        if (currentAnim.length == 2) {
+            if (validOutputs.contains(currentAnim[1])) {
+                return currentAnim[1];
+            }
+        }
+        return "down";
+    }
+    /**
+     * Check if the current (non-looping) animation has completed
+     */
+    boolean readyToPlay() {
+        if (animator.getCurrentAnimation().contains("interact")) {
+            return animator.isFinished();
+        }
+        return true;
+    }
+
+    /**
      * Starts the player walking animation in the given direction.
      *
      * @param direction "left", "right", "down" or "up"
@@ -51,6 +77,9 @@ public class PlayerAnimationController extends Component {
      * Starts the default player animation when they stop walking.
      */
     void animationWalkStop(String direction) {
+        if (!readyToPlay()) {
+            return;
+        }
         String animation = String.format("idle_%s", direction);
         if (!animator.getCurrentAnimation().equals(animation)) {
             animator.startAnimation(animation);
@@ -61,6 +90,10 @@ public class PlayerAnimationController extends Component {
      * Play the interaction animation
      */
     void animationInteract() {
-        animator.startAnimation("interact");
+        // Get the current animation name to get the facing direction
+        String animation = String.format("interact_%s", getFacingDir());
+        if (!animator.getCurrentAnimation().equals(animation)) {
+            animator.startAnimation(animation);
+        }
     }
 }
