f251b7b486e5f400c123c845702638ae081ad7ab
masoo12 <a345cde@gmail.com>
2023-08-28 17:27:23 +1000

    Added PlayerInventoryTest.java

--- source/core/src/test/com/csse3200/game/components/player/PlayerInventoryTest.java
+++ source/core/src/test/com/csse3200/game/components/player/PlayerInventoryTest.java
@@ -1,7 +1,6 @@
 package com.csse3200.game.components.player;
 import com.csse3200.game.components.items.ItemComponent;
 import com.csse3200.game.extensions.GameExtension;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.BeforeEach;
@@ -26,46 +25,39 @@ public class PlayerInventoryTest {
         player.create();
         item1 = new Entity().addComponent(new ItemComponent("Hoe"));
         item2 = new Entity().addComponent(new ItemComponent("Scythe"));
-
-
     }
 
-    @DisplayName("Checking Inventory:")
     @Test
     void checkAddInventory() {
         assertTrue(player.getComponent(InventoryComponent.class).getInventory().isEmpty());
-        inventoryComponent.addItem(item1);
+        player.getComponent(InventoryComponent.class).addItem(item1);
         verify(inventoryComponent).addItem(item1);
-        inventoryComponent.addItem(item2);
+        player.getComponent(InventoryComponent.class).addItem(item2);
         verify(inventoryComponent).addItem(item2);
-        List<Entity> testItems = new ArrayList<>();
-        testItems.add(item1);
-        testItems.add(item2);
         assertArrayEquals(new List[]{player.getComponent(InventoryComponent.class).getInventory()},
-                new List[]{testItems});
-
-
+                new List[]{inventoryComponent.getInventory()});
     }
     @Test
     void checkRemoveItem() {
         assertTrue(inventoryComponent.getInventory().isEmpty());
-        inventoryComponent.addItem(item1);
+        player.getComponent(InventoryComponent.class).addItem(item1);
         verify(inventoryComponent).addItem(any(Entity.class));
-        inventoryComponent.removeItem(item1);
+        player.getComponent(InventoryComponent.class).removeItem(item1);
         assertTrue(inventoryComponent.getInventory().isEmpty());
         verify(inventoryComponent).removeItem(any(Entity.class));
     }
     @Test
     void checkSetHeldItem() {
-        inventoryComponent.addItem(item1);
-        inventoryComponent.setHeldItem(0);
-
+        player.getComponent(InventoryComponent.class).addItem(item1);
+        player.getComponent(InventoryComponent.class).setHeldItem(0);
         assertSame(inventoryComponent.getHeldItem(), item1);
+        player.getComponent(InventoryComponent.class).removeItem(item1);
+        assertThrows(IndexOutOfBoundsException.class,()->inventoryComponent.setHeldItem(1));
     }
     @Test
     void checkGetHeldItem() {
-        inventoryComponent.addItem(item1);
-        inventoryComponent.setHeldItem(0);
+        player.getComponent(InventoryComponent.class).addItem(item1);
+        player.getComponent(InventoryComponent.class).setHeldItem(0);
         assertSame(inventoryComponent.getHeldItem(), item1);
     }
 
