e15048b72a2c541084d92617cb1db86e22c4aa52
Japamaja <Jacksontrenerry01@gmail.com>
2023-08-30 17:35:03 +1000

    Added tool animations for the player. Needs work but has been discussed with team 2 to try to resolve these issues

--- source/core/src/main/com/csse3200/game/components/player/PlayerAnimationController.java
+++ source/core/src/main/com/csse3200/game/components/player/PlayerAnimationController.java
@@ -1,6 +1,12 @@
 package com.csse3200.game.components.player;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.StringBuilder;
 import com.csse3200.game.components.Component;
+import com.csse3200.game.components.items.ItemActions;
+import com.csse3200.game.components.items.ItemComponent;
+import com.csse3200.game.entities.Entity;
 import com.csse3200.game.rendering.AnimationRenderComponent;
 
 /**
@@ -19,15 +25,68 @@ public class PlayerAnimationController extends Component {
         entity.getEvents().addListener("animationRunStart", this::animationRunStart);
         entity.getEvents().addListener("animationWalkStop", this::animationWalkStop);
         entity.getEvents().addListener("animationInteract", this::animationInteract);
+        entity.getEvents().addListener("use", this::use);
 
         animator.startAnimation("default");
     }
-    
+
+    void use(Vector2 playerPos, Vector2 mousePos, Entity itemInHand) {
+        if (itemInHand != null) {
+            if (itemInHand.getComponent(ItemComponent.class) != null) {
+
+                String animation = String.format("%s_%s", itemInHand.getComponent(ItemComponent.class).getItemName().toLowerCase(),
+                        getDirection(playerPos, mousePos));
+                if (!animator.getCurrentAnimation().equals(animation)) {
+                    animator.startAnimation(animation);
+                }
+            }
+        }
+    }
+
+    /**
+     * Gets the direction the animation should be player
+     * @param playerPos the position of the player
+     * @param mousePos the position of the mouse
+     * @return a String either up, down, left or right depending on where the mouse is in relation to the player.
+     */
+    private String getDirection(Vector2 playerPos, Vector2 mousePos) {
+        int width = Gdx.graphics.getWidth();
+        int height = Gdx.graphics.getHeight();
+
+
+        int playerXPos = width/2;
+        int playerYPos = height/2;
+
+        int xDelta = 0;
+        int yDelta = 0;
+
+        if (playerYPos + 48 < mousePos.y) {
+            return "down";
+        } else if (playerYPos - 48 > mousePos.y) {
+            return "up";
+        }
+
+        if (playerXPos - 24 > mousePos.x){
+            xDelta -= 1;
+        } else if (playerXPos + 24 < mousePos.x) {
+            xDelta += 1;
+        }
+
+        if (xDelta == -1) {
+            return "left";
+        } else if (xDelta == 1) {
+            return "right";
+        }
+        return "up";
+    }
+
     /**
      * Check if the current (non-looping) animation has completed
      */
     boolean readyToPlay() {
-        if (animator.getCurrentAnimation().contains("interact")) {
+        if (animator.getCurrentAnimation().contains("interact") || animator.getCurrentAnimation().contains("hoe")
+                || animator.getCurrentAnimation().contains("shovel") || animator.getCurrentAnimation().contains("scythe")
+                || animator.getCurrentAnimation().contains("watering_can")) {
             return animator.isFinished();
         }
         return true;
