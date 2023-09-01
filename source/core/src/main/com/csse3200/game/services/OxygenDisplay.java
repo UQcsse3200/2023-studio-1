eecbad3ed7e9b925216a72c2ea338e84d338e783
daniel-cottrell <d.cottrell@uqconnect.edu.au>
2023-09-01 18:05:25 +1000

    Added an oxygen display class to Services and added the function call descriptions.

---
+++ source/core/src/main/com/csse3200/game/services/OxygenDisplay.java
@@ -0,0 +1,37 @@
+package com.csse3200.game.services;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.csse3200.game.ui.UIComponent;
+
+/**
+ * A ui component for displaying the current oxygen level on the Main Game Screen.
+ */
+public class OxygenDisplay extends UIComponent{
+
+    @Override
+    public void create() {
+        // Implement here
+    }
+
+
+    public void updateDisplay() {
+        // Implement here
+    }
+
+    @Override
+    public void draw(SpriteBatch batch) {
+        // Does nothing since draw
+        return;
+    }
+
+
+    @Override
+    public void dispose() {
+        // Implement here
+    }
+}
