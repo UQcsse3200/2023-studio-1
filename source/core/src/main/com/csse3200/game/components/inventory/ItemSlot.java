d1dc2af973f4cdee9ac2a17434b284490db4b8a3
Tristan Duncombe <tduncombe747@gmail.com>
2023-09-01 22:09:34 +1000

    Create ItemSlot.java

---
+++ source/core/src/main/com/csse3200/game/components/inventory/ItemSlot.java
@@ -0,0 +1,36 @@
+package com.csse3200.game.components.inventory;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.csse3200.game.ui.UIComponent;
+
+import java.util.ArrayList;
+
+public class ItemSlot extends UIComponent {
+
+    public void create() {
+        super.create();
+    }
+
+    /**
+     * Creates actors and positions them on the stage using a table.
+     * @see Table for positioning options
+     */
+    private void addActors() {
+    }
+
+    /**
+     * Draw stage of inventory
+     * @param batch Batch to render to.
+     */
+    @Override
+    public void draw(SpriteBatch batch)  {
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+}
