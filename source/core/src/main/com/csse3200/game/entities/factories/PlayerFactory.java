27bf95b5fa6c54d968c46e4724f56956861ee676
pchsa <pch.sarmiento@gmail.com>
2023-09-03 22:57:43 +1000

    Added InteractionColliderComponent
    Component is responsible for detecting entities within specified range for interaction. Provides methods to filter entities based on direction, component type, and get nearest entity from a list.

--- source/core/src/main/com/csse3200/game/entities/factories/PlayerFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/PlayerFactory.java
@@ -61,7 +61,8 @@ public class PlayerFactory {
             //.addComponent(new PlayerStatsDisplay())
             .addComponent(animator)
             .addComponent(new PlayerAnimationController())
-            .addComponent(new ItemPickupComponent());
+            .addComponent(new ItemPickupComponent())
+            .addComponent(new InteractionColliderComponent(2f));
 
     PhysicsUtils.setScaledCollider(player, 0.6f, 0.3f);
     player.getComponent(ColliderComponent.class).setDensity(1.5f);
