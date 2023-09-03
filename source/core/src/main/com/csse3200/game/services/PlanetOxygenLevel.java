7d7ccdda61f1c06df45e935e8c24906edb8af847
Gil Wise <gilwise245@gmail.com>
2023-09-03 18:58:29 +1000

    Added blank methods in PlanetOxygenLevel

--- source/core/src/main/com/csse3200/game/services/PlanetOxygenLevel.java
+++ source/core/src/main/com/csse3200/game/services/PlanetOxygenLevel.java
@@ -3,6 +3,51 @@ package com.csse3200.game.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class PlanetOxygenLevel {
+public class PlanetOxygenLevel implements OxygenLevel{
     private static final Logger logger = LoggerFactory.getLogger(TimeService.class);
+    
+    @Override
+    public void setUpperLimit(int kilograms) {
+    
+    }
+    
+    @Override
+    public void addOxygen(float kilogramsToAdd) {
+    
+    }
+    
+    @Override
+    public void removeOxygen(float kilogramsToRemove) {
+    
+    }
+    
+    @Override
+    public float getOxygen() {
+        return 0;
+    }
+    
+    @Override
+    public float getOxygenPercentage() {
+        return 0;
+    }
+    
+    @Override
+    public void registerContinuousAdd(float kilogramsPerHour) {
+    
+    }
+    
+    @Override
+    public void registerContinuousConsumption(float kilogramsPerHour) {
+    
+    }
+    
+    @Override
+    public void removeContinuousAdd(float kilogramsPerHour) {
+    
+    }
+    
+    @Override
+    public void removeContinuousConsumption(float kilogramsPerHour) {
+    
+    }
 }
