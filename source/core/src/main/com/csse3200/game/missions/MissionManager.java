57f7f0d8debd8b12af6fba1d646f7aad6b8ac8dc
Lucas Hicks <lucasjhicks1@gmail.com>
2023-09-04 09:37:50 +1000

    Adds javadocs to the MissionManager class (See issue #152).

--- source/core/src/main/com/csse3200/game/missions/MissionManager.java
+++ source/core/src/main/com/csse3200/game/missions/MissionManager.java
@@ -10,16 +10,23 @@ import java.util.concurrent.atomic.AtomicReferenceArray;
 public class MissionManager {
 
 	private static final Achievement[] achievements = new Achievement[]{};
-	private static final ArrayList<Quest>  quests = new ArrayList<>();
+	private static final ArrayList<Quest> quests = new ArrayList<>();
 	private static final EventHandler events = new EventHandler();
 
+	/**
+	 * Creates the mission manager, registered all game achievements and adds a listener for hourly updates
+	 */
 	public MissionManager() {
-		for (Achievement mission: achievements) {
+		for (Achievement mission : achievements) {
 			mission.registerMission(events);
 		}
 		ServiceLocator.getTimeService().getEvents().addListener("hourUpdate", this::updateHour);
 	}
 
+	/**
+	 * Adds a quest to the list of active quests in the game.  Also registers this quest in the game
+	 * @param quest quest to be added and registered
+	 */
 	public void addQuest(Quest quest) {
 		quests.add(quest);
 		quest.registerMission(events);
@@ -27,8 +34,11 @@ public class MissionManager {
 
 	}
 
+	/**
+	 * Method that is called every in-game hour which loops through the active quests and updates their expiry time
+	 */
 	private void updateHour() {
-		for (Quest quest: quests) {
+		for (Quest quest : quests) {
 			quest.updateExpiry(1);
 		}
 
