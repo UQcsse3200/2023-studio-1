b406fa7954b7806156597f856e679ac52a1f3e52
Lucas Hicks <lucasjhicks1@gmail.com>
2023-09-03 16:10:22 +1000

    Adds basic mission manager service (See issue #152).

--- source/core/src/main/com/csse3200/game/missions/MissionManager.java
+++ source/core/src/main/com/csse3200/game/missions/MissionManager.java
@@ -1,2 +1,24 @@
-package com.csse3200.game.missions;public class MissionManager {
+package com.csse3200.game.missions;
+
+import com.csse3200.game.events.EventHandler;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicReferenceArray;
+
+public class MissionManager {
+
+	private static final ArrayList<Quest>  quests = new ArrayList<>();
+	private static final ArrayList<Achievement> achievements = new ArrayList<>();
+	private static final EventHandler events = new EventHandler();
+
+	public MissionManager() {
+		for (Mission mission: quests) {
+			mission.registerMission();
+		}
+		for (Mission mission: achievements) {
+			mission.registerMission();
+		}
+	}
+
 }
