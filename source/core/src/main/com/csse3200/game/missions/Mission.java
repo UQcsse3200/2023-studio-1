0611a19d4a6a03b18e84fa992430d5edd7dcb9c1
Tom-Strooper <tjstrooper@gmail.com>
2023-09-03 15:56:23 +1000

    Added basic implementation of Mission abstract class, currently without javadocs. Issue #150

--- source/core/src/main/com/csse3200/game/missions/Mission.java
+++ source/core/src/main/com/csse3200/game/missions/Mission.java
@@ -2,6 +2,18 @@ package com.csse3200.game.missions;
 
 public abstract class Mission {
 
+    private final String name;
 
+    public Mission(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract void registerMission();
+    public abstract boolean isCompleted();
+    public abstract String getDescription();
 
 }
