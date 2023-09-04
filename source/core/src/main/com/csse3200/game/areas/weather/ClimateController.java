afcc5ad2a3d00c66d956acc3f8ebdfa573efdb41
Lucas Hicks <lucasjhicks1@gmail.com>
2023-09-04 12:21:35 +1000

    Adds basic javadocs for the ClimateController class (See issue #151).

--- source/core/src/main/com/csse3200/game/areas/weather/ClimateController.java
+++ source/core/src/main/com/csse3200/game/areas/weather/ClimateController.java
@@ -16,18 +16,32 @@ public class ClimateController {
 		ServiceLocator.getTimeService().getEvents().addListener("hourUpdate", this::UpdateWeatherEvent);
 	}
 
+	/**
+	 * Gets the current humidity of the game world
+	 * @return current humidity value between 0 and 1
+	 */
 	public float GetHumidity() {
 		return humidity;
 	}
 
+	/**
+	 * Gets the current temperature of the game world
+	 * @return current game temperature
+	 */
 	public float GetTemperature() {
 		return temperature;
 	}
 
+	/**
+	 * Updates the values of the game's climate based on current weather events
+	 */
 	private void UpdateClimate() {
 		return;
 	}
 
+	/**
+	 * Updates all of the weather events every hour
+	 */
 	private void UpdateWeatherEvent() {
 		return;
 	}
