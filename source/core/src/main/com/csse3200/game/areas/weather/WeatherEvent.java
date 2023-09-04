8dfbdf81a6243a0f794a3ec9cfa73a14ffa38039
Lucas Hicks <lucasjhicks1@gmail.com>
2023-09-04 12:39:32 +1000

    Adds basic skeleton code for the Mission abstract class (See issue #164).

--- source/core/src/main/com/csse3200/game/areas/weather/WeatherEvent.java
+++ source/core/src/main/com/csse3200/game/areas/weather/WeatherEvent.java
@@ -1,8 +1,28 @@
 package com.csse3200.game.areas.weather;
 
+import javax.swing.plaf.SplitPaneUI;
+
 public abstract class WeatherEvent {
 
 	private int numHoursUntil;
 	private int duration;
 	private int priority;
+
+	//TODO
+	public void updateTime() {
+		return;
+	}
+
+	// TODO
+	public boolean isActive() {
+		return false;
+	}
+
+	public void addEffect(ClimateController climate) {
+		return;
+	}
+
+	public void triggerWeatherEvents() {
+		return;
+	}
 }
