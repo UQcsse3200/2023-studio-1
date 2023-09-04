8915aca2625679d2fc9ebdbcc69290c0e53bf9fe
Lucas Hicks <lucasjhicks1@gmail.com>
2023-09-04 12:18:34 +1000

    Adds a basic implementation of the weather event class (See issue #164).

--- source/core/src/main/com/csse3200/game/areas/weather/WeatherEvent.java
+++ source/core/src/main/com/csse3200/game/areas/weather/WeatherEvent.java
@@ -1,2 +1,8 @@
-package com.csse3200.game.areas.weather;public class WeatherEvent {
+package com.csse3200.game.areas.weather;
+
+public abstract class WeatherEvent {
+
+	private int numHoursUntil;
+	private int duration;
+	private int priority;
 }
