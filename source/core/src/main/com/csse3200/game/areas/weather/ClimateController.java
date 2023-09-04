92dd3da7c600a0927e9265689354b12886ed0596
Lucas Hicks <lucasjhicks1@gmail.com>
2023-09-04 12:38:51 +1000

    Fixes function naming in the ClimateController class (See issue #151).

--- source/core/src/main/com/csse3200/game/areas/weather/ClimateController.java
+++ source/core/src/main/com/csse3200/game/areas/weather/ClimateController.java
@@ -20,7 +20,7 @@ public class ClimateController {
 	 * Gets the current humidity of the game world
 	 * @return current humidity value between 0 and 1
 	 */
-	public float GetHumidity() {
+	public float getHumidity() {
 		return humidity;
 	}
 
@@ -28,21 +28,21 @@ public class ClimateController {
 	 * Gets the current temperature of the game world
 	 * @return current game temperature
 	 */
-	public float GetTemperature() {
+	public float getTemperature() {
 		return temperature;
 	}
 
 	/**
 	 * Updates the values of the game's climate based on current weather events
 	 */
-	private void UpdateClimate() {
+	private void updateClimate() {
 		return;
 	}
 
 	/**
-	 * Updates all of the weather events every hour
+	 * Updates all the weather events every hour
 	 */
-	private void UpdateWeatherEvent() {
+	private void updateWeatherEvent() {
 		return;
 	}
 
