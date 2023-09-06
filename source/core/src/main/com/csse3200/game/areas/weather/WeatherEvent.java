f6175dc17ecc3f80bce70a7676b976e74ddb179a
Shristi Gupta <guptashristi1408@gmail.com>
2023-09-06 21:56:53 +1000

    Implemented generateRandomTemperatureModifier method #164

--- source/core/src/main/com/csse3200/game/areas/weather/WeatherEvent.java
+++ source/core/src/main/com/csse3200/game/areas/weather/WeatherEvent.java
@@ -8,14 +8,18 @@ public abstract class WeatherEvent {
 	private int duration;
 	private int priority;
 	private float humidityModifier;
+	private float temperatureModifier;
 	private static final float MIN_HUMIDITY_MODIFIER = 0.4f;
 	private static final float MAX_HUMIDITY_MODIFIER = 1.6f;
+	private static final float MIN_TEMPERATURE_MODIFIER = 0.8f;
+	private static final float MAX_TEMPERATURE_MODIFIER = 1.2f;
 
 	public WeatherEvent(int numHoursUntil, int duration, int priority) {
 		this.numHoursUntil = numHoursUntil;
 		this.duration = duration;
 		this.priority = priority;
 		this.humidityModifier = generateRandomHumidityModifier();
+		this.temperatureModifier = generateRandomTemperatureModifier();
 	}
 
 	public void updateTime() {
@@ -43,6 +47,10 @@ public abstract class WeatherEvent {
 
 	private static float generateRandomHumidityModifier() {
 		return (new Random().nextFloat() * (MAX_HUMIDITY_MODIFIER - MIN_HUMIDITY_MODIFIER)) + MIN_HUMIDITY_MODIFIER;
+	}
+
+	private static float generateRandomTemperatureModifier() {
+		return (new Random().nextFloat() * (MAX_TEMPERATURE_MODIFIER - MIN_TEMPERATURE_MODIFIER)) + MIN_TEMPERATURE_MODIFIER;
 	}
 
 }
