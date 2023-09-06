a000d3bf2612b46602d8cbd8f5281ab456af9d93
Lucas Hicks <lucasjhicks1@gmail.com>
2023-09-06 11:46:36 +1000

    Adds a getEvents() method to the ClimateController class and adds an event handler to it (See issue #151)."

--- source/core/src/main/com/csse3200/game/areas/weather/ClimateController.java
+++ source/core/src/main/com/csse3200/game/areas/weather/ClimateController.java
@@ -1,6 +1,9 @@
 package com.csse3200.game.areas.weather;
 
+import com.csse3200.game.events.EventHandler;
 import com.csse3200.game.services.ServiceLocator;
+
+import java.util.ArrayList;
 
 public class ClimateController {
 
@@ -9,15 +12,21 @@ public class ClimateController {
 
 	private static final float DEFAULT_HUMIDITY = 0.5f;
 	private static final float DEFAULT_TEMPERATURE = 26.0f;
+	private static WeatherEvent currentWeatherEvent;
+	private static final ArrayList<WeatherEvent> weatherEvents = new ArrayList<>();
+	private final EventHandler events;
 
 	public ClimateController() {
 		humidity = DEFAULT_HUMIDITY;
 		temperature = DEFAULT_TEMPERATURE;
+		events = new EventHandler();
 		ServiceLocator.getTimeService().getEvents().addListener("hourUpdate", this::updateWeatherEvent);
+		ServiceLocator.getTimeService().getEvents().addListener("hourUpdate", this::determineActiveWeatherEvent);
 	}
 
 	/**
 	 * Gets the current humidity of the game world
+	 *
 	 * @return current humidity value between 0 and 1
 	 */
 	public float getHumidity() {
@@ -25,7 +34,16 @@ public class ClimateController {
 	}
 
 	/**
+	 * Returns the event handler for the Climate controller class
+	 * @return Event handler
+	 */
+	public EventHandler getEvents() {
+		return events;
+	}
+
+	/**
 	 * Gets the current temperature of the game world
+	 *
 	 * @return current game temperature
 	 */
 	public float getTemperature() {
@@ -44,6 +62,16 @@ public class ClimateController {
 	 */
 	private void updateWeatherEvent() {
 		return;
+	}
+
+	private void determineActiveWeatherEvent() {
+		int priority = 0;
+		for (WeatherEvent event : weatherEvents) {
+			if (event.isActive() && event.getPriority() > priority) {
+				currentWeatherEvent = event;
+				priority = event.getPriority();
+			}
+		}
 	}
 
 
