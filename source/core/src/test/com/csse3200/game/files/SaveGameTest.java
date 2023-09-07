e16e1fa46fb1dbf7852fe705d8b321261922ad65
LiamAnd <s4800410@student.uq.edu.au>
2023-09-07 21:17:43 +1000

    unit tests for the save/load

---
+++ source/core/src/test/com/csse3200/game/files/SaveGameTest.java
@@ -0,0 +1,81 @@
+package com.csse3200.game.files;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.badlogic.gdx.utils.Array;
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.entities.EntityType;
+import com.csse3200.game.files.SaveGame.GameState;
+
+public class SaveGameTest {
+
+    @Test
+    public void SetDay(){
+        GameState gameState = new SaveGame.GameState();
+
+        gameState.setDay(5);
+        assertEquals(5, gameState.getDay());
+
+        gameState.setDay(-1);
+        assertEquals(-1, gameState.getDay());
+
+        gameState.setDay(2147483647);
+        assertEquals(2147483647, gameState.getDay());
+    }
+
+    @Test
+    public void setHour(){
+        GameState gameState = new SaveGame.GameState();
+
+        gameState.setHour(17);
+        assertEquals(17, gameState.getHour());
+
+        gameState.setHour(-1);
+        assertEquals(-1, gameState.getHour());
+
+        gameState.setHour(2147483647);
+        assertEquals(2147483647, gameState.getHour());
+    }
+
+    @Test
+    public void setPlayer(){
+        GameState gameState = new SaveGame.GameState();
+
+        Entity playerTest = new Entity(EntityType.Player);
+
+        gameState.setPlayer(playerTest);
+        assertEquals(playerTest, gameState.getPlayer());
+    }
+
+    @Test
+    public void setEntities(){
+        GameState gameState = new SaveGame.GameState();
+
+        Entity[] entities = {new Entity(EntityType.Astrolotl), 
+                             new Entity(EntityType.Chicken),
+                             new Entity(EntityType.Cow),
+                             new Entity(EntityType.Plant)};
+
+        Array<Entity> entityTest = new Array<Entity>(entities);
+
+        gameState.setEntities(entityTest);
+        assertEquals(entityTest, gameState.getEntities());
+    }
+
+    @Test
+    public void setTiles(){
+        GameState gameState = new SaveGame.GameState();
+
+        Entity[] tiles = {new Entity(EntityType.Tile),
+                          new Entity(EntityType.Tile),
+                          new Entity(EntityType.Tile),
+                          new Entity(EntityType.Tile),};
+
+        Array<Entity> tileTest = new Array<Entity>(tiles);
+
+        gameState.setTiles(tileTest);
+        assertEquals(tileTest, gameState.getTiles());
+    }
+}
