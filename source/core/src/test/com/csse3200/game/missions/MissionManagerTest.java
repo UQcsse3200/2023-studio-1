d83c669cdd3f93e6be380d7f747a422173c9d0ae
Tom-Strooper <tjstrooper@gmail.com>
2023-09-08 14:48:28 +1000

    Added test files for the MissionManager and ItemReward classes. Issue #152, #150

---
+++ source/core/src/test/com/csse3200/game/missions/MissionManagerTest.java
@@ -0,0 +1,29 @@
+package com.csse3200.game.missions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MissionManagerTest {
+
+    @Test
+    void testAcceptQuest() {
+    }
+
+    @Test
+    void testGetActiveQuests() {
+    }
+
+    @Test
+    void testAddQuest() {
+    }
+
+    @Test
+    void testGetSelectableQuests() {
+    }
+
+    @Test
+    void testGetEvents() {
+    }
+
+}
---
+++ source/core/src/test/com/csse3200/game/missions/rewards/ItemRewardTest.java
@@ -0,0 +1,13 @@
+package com.csse3200.game.missions.rewards;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemRewardTest {
+
+    @Test
+    void testCollect() {
+    }
+
+}
