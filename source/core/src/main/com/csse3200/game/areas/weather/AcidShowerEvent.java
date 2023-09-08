347868e8e61fa13c1b074b598bfe7c42083730fc
Shristi Gupta <guptashristi1408@gmail.com>
2023-09-08 21:25:43 +1000

    Added in JavaDoc comments #164.

--- source/core/src/main/com/csse3200/game/areas/weather/AcidShowerEvent.java
+++ source/core/src/main/com/csse3200/game/areas/weather/AcidShowerEvent.java
@@ -1,11 +1,22 @@
 package com.csse3200.game.areas.weather;
 
+/**
+ * Represents rain within the game's weather system.
+ */
 public class AcidShowerEvent extends WeatherEvent {
     private static final float MIN_HUMIDITY_MODIFIER = 0.7f;
     private static final float MAX_HUMIDITY_MODIFIER = 1.3f;
     private static final float MIN_TEMPERATURE_MODIFIER = 0.7f;
     private static final float MAX_TEMPERATURE_MODIFIER = 0.9f;
 
+    /**
+     * Constructs a AcidShowerEvent with a given countdown, duration, priority and severity.
+     *
+     * @param numHoursUntil number of in-game hours until the weather event can occur
+     * @param duration      number of in-game hours that the event can occur for
+     * @param priority      priority of the weather event
+     * @param severity      intensity of the weather event, impacting its effects
+     */
 
     public AcidShowerEvent(int numHoursUntil, int duration, int priority, float severity) {
         super(numHoursUntil, duration, priority, severity);
--- source/core/src/main/com/csse3200/game/areas/weather/SolarSurgeEvent.java
+++ source/core/src/main/com/csse3200/game/areas/weather/SolarSurgeEvent.java
@@ -1,6 +1,19 @@
 package com.csse3200.game.areas.weather;
 
+/**
+ * Represents a heat wave event within the planet's weather system.
+ * SolarSurgeEvent is caused by the production of a massive amount of solar energy.
+ */
 public class SolarSurgeEvent extends WeatherEvent {
+
+    /**
+     * Constructs a SolarSurgeEvent with a given countdown, duration, priority and severity.
+     *
+     * @param numHoursUntil number of in-game hours until the weather event can occur
+     * @param duration      number of in-game hours that the event can occur for
+     * @param priority      priority of the weather event
+     * @param severity      intensity of the weather event, impacting its effects
+     */
     public SolarSurgeEvent(int numHoursUntil, int duration, int priority, float severity) {
         super(numHoursUntil, duration, priority, severity);
     }
