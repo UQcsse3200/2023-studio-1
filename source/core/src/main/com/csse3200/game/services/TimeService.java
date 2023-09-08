c4cfcb26895746f730f384e2db9f5cc5ef1c550d
Lucas Hicks <lucasjhicks1@gmail.com>
2023-09-08 15:16:39 +1000

    205 Adds javadocs for TimeService class

--- source/core/src/main/com/csse3200/game/services/TimeService.java
+++ source/core/src/main/com/csse3200/game/services/TimeService.java
@@ -21,6 +21,9 @@ public class TimeService {
 	private final EventHandler events;
 
 
+	/**
+	 * Constructs a basic TimeService instance to track the in-game time
+	 */
 	public TimeService() {
 		hour = 0;
 		day = 0;
@@ -30,48 +33,89 @@ public class TimeService {
 		events = new EventHandler();
 	}
 
+	/**
+	 * Returns whether the game is paused or not
+	 * @return boolean value representing whether the game is paused or not
+	 */
 	public boolean isPaused() {
 		return paused;
 	}
 
+	/**
+	 * Changes the pause state of the game
+	 * @param state boolean value for whether the game is paused or not
+	 */
 	public void setPaused(boolean state) {
 		paused = state;
 		ServiceLocator.getTimeSource().setTimeScale(state ? 0 : 1);
 	}
 
+	/**
+	 * Gets the current in-game hour
+	 * @return in-game hour
+	 */
 	public int getHour() {
 		return hour;
 	}
 
+	/**
+	 * Gets the current in-game day
+	 * @return in-game day
+	 */
 	public int getDay() {
 		return day;
 	}
 
+	/**
+	 * Gets the current in-game minute
+	 * @return in-game minute
+	 */
 	public int getMinute() {
 		return minute;
 	}
+
+	/**
+	 * Sets the in-game hour to a certain value. Also updates the time buffer and triggers any necessary events
+	 * @param hour in-game hour
+	 */
 	public void setHour(int hour) {
 		this.hour = hour;
 		this.timeBuffer = 0;
 		events.trigger("hourUpdate");
 	}
 
+	/**
+	 * Sets the in-game day to a certain value. Also updates the time buffer and triggers any necessary events
+	 * @param day in-game day
+	 */
 	public void setDay(int day) {
 		this.day = day;
 		this.timeBuffer= 0;
 		events.trigger("dayUpdate");
 	}
 
+	/**
+	 * Sets the in-game minute to a certain value. Also updates the time buffer and triggers any necessary events
+	 * @param minute in-game minute
+	 */
 	public void setMinute(int minute) {
 		this.minute = minute;
 		this.timeBuffer = 0;
 		events.trigger("minuteUpdate");
 	}
 
+	/**
+	 * Gets the event handler for the TimeService
+	 * @return event handler
+	 */
 	public EventHandler getEvents() {
 		return events;
 	}
 
+	/**
+	 * Tracks the in-game time stored in the time service. This method is called in the main game loop. It calculates
+	 * the time that has passed since it last checked the time and calculates whether in-game time has elapsed.
+	 */
 	public void update() {
 		// this time will be in ms
 		long timePassed = ServiceLocator.getTimeSource().getTimeSince(lastGameTime);
