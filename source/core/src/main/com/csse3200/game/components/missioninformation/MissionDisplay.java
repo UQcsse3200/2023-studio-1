8435d94d9fa82255f512e292d5f63d631971991c
Olivia Ronda <o.ronda@uqconnect.edu.au>
2023-09-08 18:12:02 +1000

    #135 #187 Updated main mission menu & added quest menu.

--- source/core/src/main/com/csse3200/game/components/missioninformation/MissionDisplay.java
+++ source/core/src/main/com/csse3200/game/components/missioninformation/MissionDisplay.java
@@ -1,15 +1,28 @@
 package com.csse3200.game.components.missioninformation;
 
-import com.csse3200.game.ui.UIComponent;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Align;
+import com.csse3200.game.missions.MissionManager;
+import com.csse3200.game.missions.quests.Quest;
 import com.csse3200.game.screens.MainGameScreen;
-import com.sun.nio.sctp.PeerAddressChangeNotification;
+import com.csse3200.game.services.ServiceLocator;
+import com.csse3200.game.ui.UIComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
+/**
+ * Renders a UI for interacting with Missions.
+ */
 public class MissionDisplay extends UIComponent {
+    private MissionManager missionManager;
 
     private Window window;
     private Window achWindow;
@@ -22,29 +35,28 @@ public class MissionDisplay extends UIComponent {
     @Override
     public void create() {
         super.create();
-        addActors();
+        missionManager = ServiceLocator.getMissionManager();
         isOpen = false;
-        entity.getEvents().addListener("toggleMissions",this::toggleOpen);
-        entity.getEvents().addListener("toggleQuests",this::toggleQuests);
-        entity.getEvents().addListener("toggleAchievements",this::toggleAcheivements);
+
+        addActors();
+
+        entity.getEvents().addListener("toggleMissions", this::toggleOpen);
+        entity.getEvents().addListener("toggleAchievements", this::toggleAchievements);
     }
 
     /**
      * Creates actors and positions them on the stage using a table.
+     *
      * @see Table for positioning options
      */
     private void addActors() {
-
-        // Create a window for the inventory using the skin
-        window = new Window("Missions", skin);
-        window.pad(40, 10, 10, 10); // Add padding to with so that the text doesn't go offscreen
-        window.add("QUEST o ACHEI p  ");
-
-        window.pack(); // Pack the window to the size
-        window.setMovable(false);
-        window.setPosition(stage.getWidth() / 2 - window.getWidth() / 2, stage.getHeight() / 2 - window.getHeight() / 2); // Center the window on the stage
+        window = new Window("Mission Giver", skin);
         window.setVisible(false);
         stage.addActor(window);
+
+        questWindow = new Window("Quests", skin);
+        questWindow.setVisible(false);
+        stage.addActor(questWindow);
 
         achWindow = new Window("Achievements", skin);
         achWindow.pad(40, 10, 10, 10); // Add padding to with so that the text doesn't go offscreen
@@ -54,52 +66,325 @@ public class MissionDisplay extends UIComponent {
         achWindow.setPosition(stage.getWidth() / 2 - achWindow.getWidth() / 2, stage.getHeight() / 2 - achWindow.getHeight() / 2); // Center the window on the stage
         achWindow.setVisible(false);
         stage.addActor(achWindow);
-
-        questWindow = new Window("Quests", skin);
-        questWindow.pad(40, 10, 10, 10); // Add padding to with so that the text doesn't go offscreen
-        questWindow.add("QUEST:1");
-
-        questWindow.pack(); // Pack the window to the size
-        questWindow.setMovable(false);
-        questWindow.setPosition(stage.getWidth() / 2 - questWindow.getWidth() / 2, stage.getHeight() / 2 - questWindow.getHeight() / 2); // Center the window on the stage
-        questWindow.setVisible(false);
-        stage.addActor(questWindow);
     }
 
     /**
-     * Draw stage of inventory
+     * Generates the main menu for missions. Contains a small blurb from the Mission NPC
+     * and allows the user to view either quests or achievements.
+     */
+    private void generateMissionMenu() {
+        window.clear();
+
+        TextButton achievementsButton = new TextButton("Achievements", skin);
+        achievementsButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent changeEvent, Actor actor) {
+                logger.debug("Achievements button clicked");
+                toggleAchievements();
+            }
+        });
+
+
+        TextButton questsButton = new TextButton("Quests", skin);
+        questsButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent changeEvent, Actor actor) {
+                logger.debug("Quests button clicked");
+                openQuests();
+            }
+        });
+
+        Table buttonTable = new Table();
+        buttonTable.row();
+        buttonTable.add(achievementsButton).pad(30f);
+        buttonTable.add(questsButton).pad(30f);
+
+        Label missionDescription = new Label(
+                "Looks like your skills could be useful for us here on Alpha Centauri. Why don't you take a look at how you can help?",
+                skin,
+                "pixel-body",
+                "black"
+        );
+        missionDescription.setWrap(true);
+        missionDescription.setAlignment(Align.center);
+
+        Table contentTable = new Table();
+        contentTable.defaults().size(500f, 50f);
+        contentTable.row().padBottom(30f).padTop(30f);
+        contentTable.add(missionDescription).center().expandX().expandY();
+        contentTable.row();
+        contentTable.add(buttonTable).center().expandX().expandY();
+
+        window.add(contentTable);
+        window.pad(50, 50, 50, 50);
+        window.pack();
+        window.setMovable(false);
+        window.setPosition(
+                stage.getWidth() / 2 - window.getWidth() / 2,
+                stage.getHeight() / 2 - window.getHeight() / 2
+        ); // center on stage
+
+    }
+
+    /**
+     * Generates the quest menu. Contains a table split into new, in progress, expired and completed quests.
+     */
+    private void generateQuestsMenu() {
+        questWindow.clear();
+
+        List<Quest> selectableQuests = missionManager.getSelectableQuests();
+        List<Quest> activeQuests = missionManager.getActiveQuests();
+
+        List<Quest> inProgressQuests = activeQuests.stream().filter(quest -> !(quest.isExpired() || quest.isCompleted())).toList();
+        List<Quest> expiredQuests = activeQuests.stream().filter(Quest::isExpired).toList();
+        List<Quest> completedQuests = activeQuests.stream().filter(Quest::isCompleted).toList();
+
+        Label newQuestsLabel = new Label(
+                "New",
+                skin,
+                "pixel-mid",
+                "black"
+        );
+        Label activeQuestsLabel = new Label(
+                "Active",
+                skin,
+                "pixel-mid",
+                "black"
+        );
+        Label expiredQuestsLabel = new Label(
+                "Expired",
+                skin,
+                "pixel-mid",
+                "black"
+        );
+        Label completedQuestsLabel = new Label(
+                "Completed",
+                skin,
+                "pixel-mid",
+                "black"
+        );
+
+        Table questTable = new Table();
+
+        questTable.row();
+        questTable.add(newQuestsLabel).colspan(4);
+
+        for (Quest quest : selectableQuests) {
+            questTable.row();
+
+            Label questName = new Label(
+                    quest.getName(),
+                    skin,
+                    "pixel-mid",
+                    "black"
+            );
+            Label questDescription = new Label(
+                    quest.getShortDescription(),
+                    skin,
+                    "pixel-body",
+                    "black"
+            );
+
+            TextButton button = new TextButton(
+                    "Accept",
+                    skin,
+                    "small"
+            );
+            button.addListener(new ChangeListener() {
+                @Override
+                public void changed(ChangeEvent changeEvent, Actor actor) {
+                    missionManager.acceptQuest(quest);
+                    generateQuestsMenu();
+                }
+            });
+            TextButton viewButton = new TextButton(
+                    "View",
+                    skin,
+                    "small"
+            );
+
+            questTable.add(questName).right().pad(10f);
+            questTable.add(questDescription).pad(10f);
+            questTable.add(button).pad(10f);
+            questTable.add(viewButton).pad(10f);
+        }
+
+        questTable.row();
+        questTable.add(activeQuestsLabel).colspan(4);
+
+        for (Quest quest : inProgressQuests) {
+            Label questName = new Label(
+                    quest.getName(),
+                    skin,
+                    "pixel-mid",
+                    "black"
+            );
+            Label questDescription = new Label(
+                    quest.getShortDescription(),
+                    skin,
+                    "pixel-body",
+                    "black"
+            );
+
+            TextButton viewButton = new TextButton(
+                    "View",
+                    skin,
+                    "small"
+            );
+
+            questTable.row();
+            questTable.add(questName).right().pad(10f);
+            questTable.add(questDescription).pad(10f);
+            questTable.add();
+            questTable.add(viewButton).pad(10f);
+        }
+
+        questTable.row();
+        questTable.add(expiredQuestsLabel).colspan(4);
+
+        for (Quest quest : expiredQuests) {
+            Label questName = new Label(
+                    quest.getName(),
+                    skin,
+                    "pixel-mid",
+                    "black"
+            );
+            Label questDescription = new Label(
+                    quest.getShortDescription(),
+                    skin,
+                    "pixel-body",
+                    "black"
+            );
+
+            questTable.row();
+            questTable.add(questName).right().pad(10f);
+            questTable.add(questDescription).pad(10f);
+
+            TextButton button = new TextButton(
+                    "Reactivate",
+                    skin,
+                    "small"
+            );
+            button.addListener(new ChangeListener() {
+                @Override
+                public void changed(ChangeEvent changeEvent, Actor actor) {
+                    quest.resetExpiry();
+                    generateQuestsMenu();
+                }
+            });
+
+            questTable.add(button).pad(10f);
+
+            TextButton viewButton = new TextButton(
+                    "View",
+                    skin,
+                    "small"
+            );
+
+            questTable.add(viewButton).pad(10f);
+        }
+
+        questTable.row();
+        questTable.add(completedQuestsLabel).colspan(4);
+
+        for (Quest quest : completedQuests) {
+            Label questName = new Label(
+                    quest.getName(),
+                    skin,
+                    "pixel-mid",
+                    "black"
+            );
+            Label questDescription = new Label(
+                    quest.getShortDescription(),
+                    skin,
+                    "pixel-body",
+                    "black"
+            );
+
+            questTable.row();
+            questTable.add(questName).right().pad(10f);
+            questTable.add(questDescription).pad(10f);
+
+            TextButton button = new TextButton(
+                    "Collect Reward",
+                    skin,
+                    "small"
+            );
+            button.addListener(new ChangeListener() {
+                @Override
+                public void changed(ChangeEvent changeEvent, Actor actor) {
+                    quest.collectReward();
+                }
+            });
+
+            questTable.add(button).pad(10f);
+
+            TextButton viewButton = new TextButton(
+                    "View",
+                    skin,
+                    "small"
+            );
+
+            questTable.add(viewButton).pad(10f);
+        }
+
+        questWindow.add(questTable);
+        questWindow.pad(50, 50, 50, 50);
+        questWindow.pack();
+        questWindow.setMovable(false);
+        questWindow.setPosition(
+                stage.getWidth() / 2 - questWindow.getWidth() / 2,
+                stage.getHeight() / 2 - questWindow.getHeight() / 2
+        ); // center on stage
+    }
+
+    /**
+     * Draw stage, unused for now.
+     *
      * @param batch Batch to render to.
      */
     @Override
-    public void draw(SpriteBatch batch)  {
+    public void draw(SpriteBatch batch) {
     }
 
-    public void toggleOpen(){
+    /**
+     * Toggles the visibility of the main mission ui.
+     */
+    public void toggleOpen() {
         if (isOpen) {
             window.setVisible(false);
             achWindow.setVisible(false);
             questWindow.setVisible(false);
+
             isOpen = false;
         } else {
+            generateMissionMenu();
+
             window.setVisible(true);
             isOpen = true;
         }
     }
-    public void toggleQuests(){
-        if (isOpen && achOpen) {
+
+    /**
+     * Opens the quest menu.
+     */
+    public void openQuests() {
+        if (isOpen) {
+            generateQuestsMenu();
+
+            questWindow.setVisible(true);
             window.setVisible(false);
             achWindow.setVisible(false);
-            questWindow.setVisible(true);
-            questsOpen = true;
-            achOpen = false;
-        } else if (isOpen) {
-            window.setVisible(false);
-            questWindow.setVisible(true);
+
             questsOpen = true;
             achOpen = false;
         }
     }
-    public void toggleAcheivements(){
+
+    /**
+     * Opens the achievement menu.
+     */
+    public void toggleAchievements() {
         if (isOpen && questsOpen) {
             window.setVisible(false);
             questWindow.setVisible(false);
@@ -113,8 +398,16 @@ public class MissionDisplay extends UIComponent {
             achOpen = true;
         }
     }
+
+    /**
+     * Cleans up.
+     */
     @Override
     public void dispose() {
+        window.clear();
+        questWindow.clear();
+        achWindow.clear();
+
         super.dispose();
     }
 }
