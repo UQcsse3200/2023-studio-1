20bbeb0d0ac67a2d8b4f71023ba005c220e8bd49
Shristi Gupta <guptashristi1408@gmail.com>
2023-09-09 13:51:33 +1000

    Completed 100% testing for SolarSurgeEvent #164.

--- source/core/src/test/com/csse3200/game/areas/weather/WeatherEventTest.java
+++ source/core/src/test/com/csse3200/game/areas/weather/WeatherEventTest.java
@@ -11,7 +11,7 @@ public class WeatherEventTest {
 
     private WeatherEvent weatherEvent1, weatherEvent2, weatherEvent3, weatherEvent4, weatherEvent5;
     private AcidShowerEvent acidShowerEvent1, acidShowerEvent2, acidShowerEvent3, acidShowerEvent4, acidShowerEvent5;
-    private SolarSurgeEvent solarSurgeEvent1, solarSurgeEvent2;
+    private SolarSurgeEvent solarSurgeEvent1, solarSurgeEvent2, solarSurgeEvent3, solarSurgeEvent4, solarSurgeEvent5;
 
     @BeforeEach
     public void setUp() {
@@ -25,8 +25,11 @@ public class WeatherEventTest {
         acidShowerEvent3 = new AcidShowerEvent(2, 4, 5, 1.0f);
         acidShowerEvent4 = new AcidShowerEvent(3, 3, 3, 1.3f);
         acidShowerEvent5 = new AcidShowerEvent(5, 5, 1, 1.1f);
-        solarSurgeEvent1 = new SolarSurgeEvent(0, 6, 1, 1.1f);
-        solarSurgeEvent2= new SolarSurgeEvent(3, 3, 3, 1.3f);
+        solarSurgeEvent1 = new SolarSurgeEvent(0, 9, 1, 1.2f);
+        solarSurgeEvent2= new SolarSurgeEvent(1, 2, 2, 1.4f);
+        solarSurgeEvent3 = new SolarSurgeEvent(2, 4, 5, 1.0f);
+        solarSurgeEvent4= new SolarSurgeEvent(3, 3, 3, 1.3f);
+        solarSurgeEvent5 = new SolarSurgeEvent(5, 5, 1, 1.1f);
     }
 
     @Test
@@ -117,5 +120,23 @@ public class WeatherEventTest {
         assertEquals(-10.0f, acidShowerEvent3.getTemperatureModifier(), 0.00001);
         assertEquals(-11.5f, acidShowerEvent4.getTemperatureModifier(), 0.00001);
         assertEquals(-10.5f, acidShowerEvent5.getTemperatureModifier(), 0.00001);
+    }
+
+    @Test
+    public void testSolarSurgeEventGetHumidityModifier() {
+        assertEquals(-0.44f, solarSurgeEvent1.getHumidityModifier(), 0.00001);
+        assertEquals(-0.48f, solarSurgeEvent2.getHumidityModifier(), 0.00001);
+        assertEquals(-0.4f, solarSurgeEvent3.getHumidityModifier(), 0.00001);
+        assertEquals(-0.46f, solarSurgeEvent4.getHumidityModifier(), 0.00001);
+        assertEquals(-0.42f, solarSurgeEvent5.getHumidityModifier(), 0.00001);
+    }
+
+    @Test
+    public void testSolarSurgeEventGetTemperatureModifier() {
+        assertEquals(33.0f, solarSurgeEvent1.getTemperatureModifier(), 0.00001);
+        assertEquals(36.0f, solarSurgeEvent2.getTemperatureModifier(), 0.00001);
+        assertEquals(30.0f, solarSurgeEvent3.getTemperatureModifier(), 0.00001);
+        assertEquals(34.5f, solarSurgeEvent4.getTemperatureModifier(), 0.00001);
+        assertEquals(31.5f, solarSurgeEvent5.getTemperatureModifier(), 0.00001);
     }
 }
