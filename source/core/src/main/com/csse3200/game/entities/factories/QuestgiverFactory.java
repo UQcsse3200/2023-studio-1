1efd4864ac2b96c6ffe3f67449d2dfe338742e60
yetanothercarbot <55005345+yetanothercarbot@users.noreply.github.com>
2023-09-10 23:17:06 +1000

    Add QuestIndicatorComponent and QuestgiverIndicator

--- source/core/src/main/com/csse3200/game/entities/factories/QuestgiverFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/QuestgiverFactory.java
@@ -29,6 +29,17 @@ public class QuestgiverFactory {
     return questgiver;
   }
 
+  public static Entity createQuestgiverIndicator(Entity questgiver) {
+
+    AnimationRenderComponent animator = setupMissionAnimations();
+
+    Entity questgiverIndicator = new Entity(EntityType.QuestgiverIndicator)
+            .addComponent(animator);
+
+    questgiverIndicator.getComponent(AnimationRenderComponent.class).scaleEntity();
+    return questgiverIndicator;
+  }
+
   /**
    * Adds all animations to the AnimationRenderComponent for the questgiver
    *
@@ -41,6 +52,20 @@ public class QuestgiverFactory {
     animator.addAnimation("default", 0.1f, Animation.PlayMode.LOOP);
     animator.startAnimation("default");
     // this will get updated in a future sprint to include proper animations
+    return animator;
+  }
+
+  /**
+   *  Load animations for the questgiver indicator animation
+   *
+   * @return an AnimationRenderComponent for the questgiver indicator animations
+   */
+  private static AnimationRenderComponent setupMissionAnimations() {
+    AnimationRenderComponent animator = new AnimationRenderComponent(
+            ServiceLocator.getResourceService().getAsset("images/missionStatus.atlas", TextureAtlas.class),
+            16f);
+
+    animator.addAnimation("reward_available", 100);
     return animator;
   }
 }
