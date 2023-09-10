7e14020becb2c6ca9fd33664af431c32d82ba111
Japamaja <Jacksontrenerry01@gmail.com>
2023-09-10 21:39:17 +1000

    #125 Adds some tests to SaveGameTest and SaveLoadServiceTest

--- source/core/src/test/com/csse3200/game/files/SaveGameTest.java
+++ source/core/src/test/com/csse3200/game/files/SaveGameTest.java
@@ -78,4 +78,29 @@ public class SaveGameTest {
     gameState.setTiles(tileTest);
     assertEquals(tileTest, gameState.getTiles());
   }
+
+  @Test
+  public void testFilterTiles() {
+    Array<Entity> entities = new Array<Entity>();
+    Entity tile = new Entity(EntityType.Tile);
+    entities.add(new Entity(EntityType.Plant), new Entity(), tile, new Entity(EntityType.Chicken));
+    GameState state = new GameState();
+    state.setTiles(entities);
+    assertEquals(1,state.getTiles().size);
+    assertEquals(tile,state.getTiles().get(0));
+  }
+
+  @Test
+  public void testFilterEntities() {
+    Array<Entity> entities = new Array<Entity>();
+    Entity tile = new Entity(EntityType.Tile);
+    Entity plant = new Entity(EntityType.Plant);
+    Entity chicken = new Entity(EntityType.Chicken);
+    entities.add(plant, new Entity(), tile, chicken);
+    GameState state = new GameState();
+    state.setEntities(entities);
+    assertEquals(2,state.getEntities().size);
+    assertEquals(plant,state.getEntities().get(0));
+    assertEquals(chicken,state.getEntities().get(1));
+  }
 }
