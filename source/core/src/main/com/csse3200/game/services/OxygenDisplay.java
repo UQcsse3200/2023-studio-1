56db894b5939a6ff25f0512fbd68c2c6bc776cf4
daniel-cottrell <d.cottrell@uqconnect.edu.au>
2023-09-11 17:14:26 +1000

    Improved the display efficiency, to reuse textures rather than recreating them each run through the update display.

--- source/core/src/main/com/csse3200/game/services/OxygenDisplay.java
+++ source/core/src/main/com/csse3200/game/services/OxygenDisplay.java
@@ -8,19 +8,22 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Array;
 import com.csse3200.game.ui.UIComponent;
+
+import java.security.Provider;
 
 /**
  * A ui component for displaying the current oxygen level on the Main Game Screen.
  */
 public class OxygenDisplay extends UIComponent{
 
-    Table table;
-    Group group;
+    Table table = new Table();
+    Group group = new Group();
     private Image oxygenOutline;
     private Image oxygenFill;
+    private Array<Label> oxygenLabels;
     private Label oxygenLabel;
-    private Skin oxygenSkin;
 
     /**
      * Creates reusable ui styles and adds actors to the stage.
@@ -28,47 +31,73 @@ public class OxygenDisplay extends UIComponent{
     @Override
     public void create() {
         super.create();
-        oxygenSkin = new Skin(Gdx.files.internal("flat-earth/skin/flat-earth-ui.json"));
 
         // Adds a listener to check for oxygen updates
         ServiceLocator.getPlanetOxygenService().getEvents()
                 .addListener("oxygenUpdate", this::updateDisplay);
 
-
         updateDisplay();
     }
 
     /**
-     * Updates the display, showing the oxygen bar in the top of the main game screen.
+     * Initialises all the possible images and labels that will be used by
+     * the class, and stores them in an array to be called when needed.
      */
-    public void updateDisplay() {
-        int oxygenPercent = ServiceLocator.getPlanetOxygenService().getOxygenPercentage();
-        float scaling = (float) oxygenPercent / 100;
-
-        if (oxygenPercent == 0) {
-            //ServiceLocator.getGameArea().getPlayer().getEvents().trigger("loseScreen");
-        }
-        if (oxygenOutline != null && oxygenFill != null && oxygenLabel != null) {
-            dispose();
-        }
-
-        table = new Table();
-        group = new Group();
-        table.top();
-        table.setFillParent(true);
-        table.padTop(-130f).padLeft(-180f);
+    public void createTexture() {
+        Skin oxygenSkin = new Skin(Gdx.files.internal("flat-earth/skin/flat-earth-ui.json"));
 
         oxygenOutline = new Image(ServiceLocator.getResourceService().getAsset(
             "images/oxygen_ui/oxygen_outline.png", Texture.class));
         oxygenFill = new Image(ServiceLocator.getResourceService().getAsset(
             "images/oxygen_ui/oxygen_fill.png", Texture.class));
 
+        oxygenLabels = new Array<>();
+        for (int i = 0; i <= 100; i++) {
+            oxygenLabels.add(new Label(String.format("Oxygen: %d%%", i), oxygenSkin));
+        }
+    }
+
+    /**
+     * Updates the display, showing the oxygen bar in the top of the main game screen.
+     */
+    public void updateDisplay() {
+        if (oxygenLabels == null) {
+            createTexture();
+        }
+
+        int oxygenPercent = ServiceLocator.getPlanetOxygenService().getOxygenPercentage();
+        float scaling = (float) oxygenPercent / 100;
+
+        if (oxygenPercent == 0) {
+            ServiceLocator.getGameArea().getPlayer().getEvents().trigger("loseScreen");
+        }
+
         // Accounts for scaling of the oxygen bar due to the oxygen percent
         oxygenFill.setX(oxygenFill.getImageX() + 14 * (1 - scaling));
         oxygenFill.setScaleX(scaling);
 
-        oxygenLabel = new Label(String.format("Oxygen: %d%%", oxygenPercent), oxygenSkin);
-        oxygenLabel.setPosition(oxygenOutline.getImageX() + 125f, oxygenOutline.getImageY() + 8.5f);
+        // Add a safety check to ensure that the array is always accessed at a possible index
+        if (0 <= oxygenPercent && oxygenPercent <= 100) {
+            oxygenLabel = oxygenLabels.get(oxygenPercent);
+            oxygenLabel.setPosition(oxygenOutline.getImageX() + 125f, oxygenOutline.getImageY() + 8.5f);
+        }
+
+        // Uncomment line below to test that oxygen percent increases by 1% per hour.
+        //ServiceLocator.getPlanetOxygenService().addOxygen(10);
+    }
+
+    /**
+     * Draws the table and group onto the main game screen. Adds the oxygen elements onto the stage.
+     * @param batch Batch to render to.
+     */
+    @Override
+    public void draw(SpriteBatch batch) {
+        table.clear();
+        group.clear();
+
+        table.top();
+        table.setFillParent(true);
+        table.padTop(-130f).padLeft(-180f);
 
         group.addActor(oxygenOutline);
         group.addActor(oxygenFill);
@@ -78,13 +107,9 @@ public class OxygenDisplay extends UIComponent{
         stage.addActor(table);
     }
 
-    @Override
-    public void draw(SpriteBatch batch) {
-        // Does nothing since draw
-        return;
-    }
-
-
+    /**
+     * Removes all entities from the screen. Releases all resources from this class.
+     */
     @Override
     public void dispose() {
         super.dispose();
