1972e47ddd7d4c7b162a9c5acfb34457969e6ee1
MatildaDamman <dammanmatilda@gmail.com>
2023-09-12 14:34:05 +1000

    Added the tests for pausingAdded the tests for pausing

--- source/core/src/main/com/csse3200/game/areas/GameArea.java
+++ source/core/src/main/com/csse3200/game/areas/GameArea.java
@@ -119,4 +119,7 @@ public abstract class GameArea implements Disposable {
   public abstract Entity getTractor();
 
   public abstract GameMap getMap();
+
+  public void setPlayer(Entity customPlayer) { player = customPlayer; }
+
 }
