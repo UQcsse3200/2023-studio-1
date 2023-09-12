60f43d006cfc311626a8695ac703ae1601aafc8a
yetanothercarbot <55005345+yetanothercarbot@users.noreply.github.com>
2023-09-12 10:48:40 +1000

    Add missing comments

--- source/core/src/main/com/csse3200/game/components/npc/QuestIndicatorComponent.java
+++ source/core/src/main/com/csse3200/game/components/npc/QuestIndicatorComponent.java
@@ -25,6 +25,10 @@ public class QuestIndicatorComponent extends Component {
     Entity questgiver;
     AnimationRenderComponent parentAnimator;
 
+    /**
+     * This will create the questgiver indicator - it listens to events on the mission manager
+     * to discover when missions become available, are completed or expire.
+     */
     public void create() {
         super.create();
         this.parentAnimator = this.entity.getComponent(AnimationRenderComponent.class);
@@ -37,28 +41,48 @@ public class QuestIndicatorComponent extends Component {
         manager.getEvents().addListener("questNew", this::displayNewQuest);
     }
 
+    /**
+     * Register the questgiver. This is needed to be able to dismiss the indicator when the player interacts
+     * with the questgiver.
+     */
     public void registerQuestgiver(Entity questgiver) {
         this.questgiver = questgiver;
         questgiver.getEvents().addListener("toggleMissions", this::markRead);
     }
 
+    /**
+     * Display the reward available animation.
+     */
     private void displayFinishedQuest() {
         updateState(State.REWARD_AVAILABLE);
     }
 
+    /**
+     * Display the quest expired animation.
+     */
     private void displayExpiredQuest() {
         updateState(State.OUT_OF_TIME);
     }
 
+    /**
+     * Display the new quest animation.
+     */
     private void displayNewQuest() {
         updateState(State.QUEST_AVAILABLE);
     }
 
+    /**
+     * Mark as read - hide any animation.
+     */
     private void markRead() {
         updateState(State.IDLE);
     }
 
-    public void updateState(State newState) {
+    /**
+     * Show a given animation. 
+     * @param newState New animation state
+     */
+    private void updateState(State newState) {
         this.state = newState;
         parentAnimator.startAnimation(newState.animationName);
     }
