a6469e9f41e5b32e36d49fdc157903c6e8b489d5
Lucas Hicks <lucasjhicks1@gmail.com>
2023-09-13 22:11:02 +1000

    209 Adds javadocs to component.

--- source/core/src/main/com/csse3200/game/components/AuraLightComponent.java
+++ source/core/src/main/com/csse3200/game/components/AuraLightComponent.java
@@ -10,15 +10,53 @@ public class AuraLightComponent extends Component{
 	private final PointLight light;
 	private boolean active;
 
+	/**
+	 * Creates a AuraLightComponent which can be attached to an entity to enable it to produce light.
+	 */
 	public AuraLightComponent() {
-		light = new PointLight(ServiceLocator.getLightService().getRayHandler(), 10);
+		light = new PointLight(ServiceLocator.getLightService().getRayHandler(), 25);
 		light.setDistance(4);
 		light.setColor(DEFAULT_COLOR);
 		light.setStaticLight(true);
-		light.setXray(true); // Stops most of shadows and reduces CPU burden
+		light.setXray(true); // Stops most of the shadows and reduces CPU burden
 		light.setSoft(true);
 	}
 
+	/**
+	 * Creates an AuraLightComponent which can be attached to an entity to enable it to produce light.
+	 * @param distance distance/spread of the light
+	 */
+	public AuraLightComponent(float distance) {
+		this();
+		setDistance(distance);
+	}
+
+
+	/**
+	 *
+	 * Creates an AuraLightComponent which can be attached to an entity to enable it to produce light.
+	 * @param color color of the light being created
+	 */
+	public AuraLightComponent(Color color) {
+		this();
+		setColor(color);
+	}
+
+	/**
+	 *
+	 * Creates an AuraLightComponent which can be attached to an entity to enable it to produce light.
+	 * @param distance distance/spread of the light
+	 * @param color color of the light being created
+	 */
+	public AuraLightComponent(float distance, Color color) {
+		this();
+		setDistance(distance);
+		setColor(color);
+	}
+
+	/**
+	 * Creates the event listener for the light to enable it to be toggled on and off
+	 */
 	@Override
 	public void create() {
 		super.create();
@@ -26,44 +64,49 @@ public class AuraLightComponent extends Component{
 		entity.getEvents().addListener("toggleLight", this::toggleLight);
 	}
 
-	public AuraLightComponent(float distance) {
-		this();
-		setDistance(distance);
-	}
-
-	public AuraLightComponent(Color color) {
-		this();
-		setColor(color);
-	}
-
-	public AuraLightComponent(float distance, Color color) {
-		this();
-		setDistance(distance);
-		setColor(color);
-	}
-
+	/**
+	 * Sets the distance/spread of the light
+	 * @param distance distance/spread of the light
+	 */
 	public void setDistance(float distance) {
 		light.setDistance(distance);
 	}
 
+	/**
+	 * Sets the color of the light
+	 * @param color light color
+	 */
 	public void setColor(Color color) {
 		light.setColor(color);
 	}
 
+	/**
+	 * Toggles the light between an on and off state
+	 */
 	public void toggleLight() {
 		active = !active;
 		light.setActive(active);
 	}
 
+	/**
+	 * Determines whether or not the light is currently on
+	 * @return state of the light (on/off)
+	 */
 	public boolean getActive() {
 		return light.isActive();
 	}
 
+	/**
+	 * Called every frame draw to ensure that the position of the light is the same as the entity
+	 */
 	@Override
 	public void update() {
 		light.setPosition(entity.getCenterPosition());
 	}
 
+	/**
+	 * Disposes of the light if the entity is disposed
+	 */
 	@Override
 	public void dispose() {
 		light.dispose();
