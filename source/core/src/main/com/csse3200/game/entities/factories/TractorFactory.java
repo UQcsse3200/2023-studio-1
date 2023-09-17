c6aba92448bafdb8c655d043ab703dace58214de
Olivia Ronda <o.ronda@uqconnect.edu.au>
2023-09-17 10:49:37 +1000

    #297 Updated tractor collider

--- source/core/src/main/com/csse3200/game/entities/factories/TractorFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/TractorFactory.java
@@ -41,7 +41,7 @@ public class TractorFactory {
     tractor.getComponent(AnimationRenderComponent.class).scaleEntity();
     tractor.getComponent(TractorActions.class).setPlayer(player);
     tractor.getComponent(KeyboardTractorInputComponent.class).setActions(tractor.getComponent(TractorActions.class));
-    tractor.getComponent(ColliderComponent.class).setAsBox(new Vector2(3.5f, 1.5f), new Vector2(3f, 0.75f));
+    tractor.getComponent(ColliderComponent.class).setAsBox(new Vector2(2.25f, 1f), new Vector2(2.5625f, 1.4375f));
     tractor.getComponent(ColliderComponent.class).setDensity(999);    //prevents entities from pushing the tractor so easily
     return tractor;
   }
