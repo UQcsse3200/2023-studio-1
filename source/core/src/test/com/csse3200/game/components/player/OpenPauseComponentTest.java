256f4bdbba848314ac192b7bda24aa3c417e89c2
MatildaDamman <dammanmatilda@gmail.com>
2023-09-20 13:01:19 +1000

    new pause test

---
+++ source/core/src/test/com/csse3200/game/components/player/OpenPauseComponentTest.java
@@ -0,0 +1,82 @@
+package com.csse3200.game.components.player;
+
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.csse3200.game.areas.GameArea;
+import com.csse3200.game.components.gamearea.GameAreaDisplay;
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.extensions.GameExtension;
+import com.csse3200.game.rendering.RenderService;
+import com.csse3200.game.services.ResourceService;
+import com.csse3200.game.services.ServiceLocator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(GameExtension.class)
+@ExtendWith(MockitoExtension.class)
+class OpenPauseComponentTest {
+
+    Entity player;
+    OpenPauseComponent openPauseComponent;
+
+    @BeforeEach
+    void init() {
+        AssetManager assetManager = spy(AssetManager.class);
+        ResourceService resourceService = new ResourceService(assetManager);
+        ServiceLocator.registerResourceService(resourceService);
+        GameArea gameArea = spy(GameArea.class);
+        RenderService renderService = new RenderService();
+        renderService.setStage(mock(Stage.class));
+        GameAreaDisplay playerGuidArea = new GameAreaDisplay("");
+        ServiceLocator.registerGameArea(gameArea);
+        ServiceLocator.registerRenderService(renderService);
+        playerGuidArea.create();
+        player = new Entity();
+        openPauseComponent = new OpenPauseComponent();
+        player.addComponent(openPauseComponent);
+        player.create();
+        gameArea.setPlayer(player);
+    }
+
+    @Test
+    void shouldAllBeClosed() {
+
+        assertFalse(openPauseComponent.getPauseOpen());
+    }
+
+    @Test
+    void shouldOpenPauseMenu() {
+
+        player.getEvents().trigger("escInput");
+        assertTrue(openPauseComponent.getPauseOpen());
+    }
+
+    @Test
+    void shouldOpenPauseMenuInventoryToggle() {
+
+        player.getEvents().trigger("escInput");
+        assertTrue(openPauseComponent.getPauseOpen());
+    }
+
+    @Test
+    void shouldClosePauseMenu() {
+
+        player.getEvents().trigger("escInput");
+        player.getEvents().trigger("escInput");
+        assertFalse(openPauseComponent.getPauseOpen());
+    }
+
+    @Test
+    void shouldClosePauseMenuInventoryToggle() {
+
+        player.getEvents().trigger("escInput");
+        player.getEvents().trigger("escInput");
+        assertFalse(openPauseComponent.getPauseOpen());
+    }
+}
