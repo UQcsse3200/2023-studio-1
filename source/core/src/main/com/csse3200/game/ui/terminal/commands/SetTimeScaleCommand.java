92ef3cfc4006f4507519128b9d663d96b715d82f
Lucas Hicks <lucasjhicks1@gmail.com>
2023-09-22 00:29:18 +1000

    #396 Adds javadocs to setTimeScaleCommand

--- source/core/src/main/com/csse3200/game/ui/terminal/commands/SetTimeScaleCommand.java
+++ source/core/src/main/com/csse3200/game/ui/terminal/commands/SetTimeScaleCommand.java
@@ -8,42 +8,49 @@ import java.util.ArrayList;
 
 public class SetTimeScaleCommand implements Command {
 
-  private static final Logger logger = LoggerFactory.getLogger(SetTimeScaleCommand.class);
-  private static final int MIN_TIME = 0;
-  private static final int MAX_TIME = 23;
+	private static final Logger logger = LoggerFactory.getLogger(SetTimeScaleCommand.class);
+	private static final int MIN_TIME = 0;
+	private static final int MAX_TIME = 23;
 
-  /**
-   * Adjusts the current game time to the specified value
-   *
-   * @param args command args
-   * @return command was successful
-   */
-  @Override
-  public boolean action(ArrayList<String> args) {
-    if (!isValid(args)) {
-      logger.debug("Invalid arguments received for 'setTimeScale' command: {}", args);
-      return false;
-    }
-    ServiceLocator.getTimeSource().setTimeScale(Float.parseFloat(args.get(0)));
-    return true;
-  }
+	/**
+	 * Adjusts the current game time to the specified value
+	 *
+	 * @param args command args
+	 * @return command was successful
+	 */
+	@Override
+	public boolean action(ArrayList<String> args) {
+		if (!isValid(args)) {
+			logger.debug("Invalid arguments received for 'setTimeScale' command: {}", args);
+			return false;
+		}
+		ServiceLocator.getTimeSource().setTimeScale(Float.parseFloat(args.get(0)));
+		return true;
+	}
 
-  boolean isValid(ArrayList<String> args) {
-    if (args.size() != 1) {
-      logger.warn("Only 1 argument is needed and {} were given", args.size());
-      return false;
-    }
-    float timeScale;
-    try {
-      timeScale = Float.parseFloat(args.get(0));
-    } catch (NumberFormatException e) {
-      logger.warn("Argument provided was not a valid integer");
-      return false;
-    }
-    if (timeScale <= 0) {
-      logger.warn("Timescale value given must be greater than 0");
-      return false;
-    }
-    return true;
-  }
+	/**
+	 * Checks whether the args given to the command are valid. There must only be one argument which is a valid float
+	 * greater than 0
+	 *
+	 * @param args arguments given to command
+	 * @return whether the arguments are valid
+	 */
+	boolean isValid(ArrayList<String> args) {
+		if (args.size() != 1) {
+			logger.warn("Only 1 argument is needed and {} were given", args.size());
+			return false;
+		}
+		float timeScale;
+		try {
+			timeScale = Float.parseFloat(args.get(0));
+		} catch (NumberFormatException e) {
+			logger.warn("Argument provided was not a valid integer");
+			return false;
+		}
+		if (timeScale <= 0) {
+			logger.warn("Timescale value given must be greater than 0");
+			return false;
+		}
+		return true;
+	}
 }
