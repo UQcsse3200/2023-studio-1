f20a1feb6aeb51e4844270c651a7395dba45b0aa
adamFittlerUQ <a.fittler@uqconnect.edu.au>
2023-09-22 09:44:49 +1000

    Adding scrolling affect for the credits screen

--- source/core/src/main/com/csse3200/game/components/endcredits/EndCreditsDisplay.java
+++ source/core/src/main/com/csse3200/game/components/endcredits/EndCreditsDisplay.java
@@ -26,8 +26,6 @@ public class EndCreditsDisplay extends UIComponent {
     private Table table;
     private final GdxGame game;
 
-    private boolean hasWon;
-
     private TextButton returnButton;
 
     /**
@@ -39,7 +37,7 @@ public class EndCreditsDisplay extends UIComponent {
     /**
      * The speed in pixels/frame that the background and the planet should move at.
      */
-    private float spaceSpeed = 0.5f;
+    private float spaceSpeed = 50.0f;
 
     /**
      * The distance away from the top the text that the planet should stop in pixels.
@@ -74,31 +72,12 @@ public class EndCreditsDisplay extends UIComponent {
     }
 
     private void addActors() {
-        this.hasWon = true;
         background = new Image(new Texture(Gdx.files.internal("images/intro_background_v2.png")));
         background.setPosition(0, 0);
         float scaledHeight = Gdx.graphics.getWidth() * (background.getHeight() / background.getWidth());
         background.setHeight(scaledHeight);
 
-        // Load the animated planet
-        if (this.hasWon) {
-            planet = new Image(new Texture(Gdx.files.internal("images/earth_image.png")));
-        } else {
-            planet = new Image(new Texture(Gdx.files.internal("images/dead_planet.png")));
-        }
-
-
-        // Scale it to a 10% of screen width with a constant aspect ratio
-        float planetWidth = (float) (Gdx.graphics.getWidth() * 0.1);
-        float planetHeight = planetWidth * (planet.getHeight() / planet.getWidth());
-        planet.setSize(planetWidth, planetHeight); // Set to a reasonable fixed size
-
-        // The planet's speed is variable, it adjusts itself to make the planet appear above the text at the right time
-        // The planet is placed at some offset above the screen in the center of the screen
-        float planetOffset = 2500;
-        planet.setPosition((float)Gdx.graphics.getWidth()/2, planetOffset, Align.center);
-
-        String credits = getCredits(this.hasWon);
+        String credits = getCredits();
 
         storyLabel = new TypingLabel(credits, skin); // Create the TypingLabel with the formatted story
         storyLabel.setAlignment(Align.center); // Center align the text
@@ -121,20 +100,18 @@ public class EndCreditsDisplay extends UIComponent {
 
         rootTable.row();
 
-        rootTable.add(storyLabel).expandX().center().padTop(150f); // The story label is at the top of the screen
+        rootTable.add(storyLabel).top().padTop(Gdx.graphics.getHeight());
         rootTable.row().padTop(30f);
         rootTable.add(returnButton).bottom().padBottom(30f);
 
         // The background and planet are added directly to the stage so that they can be moved and animated freely.
         stage.addActor(background);
-        stage.addActor(planet);
         stage.addActor(rootTable);
     }
 
-    private String getCredits(boolean hasWon) {
-        String credits = "";
-        if (hasWon) {
-            credits = """
+    private String getCredits() {
+
+        return """
                 {WAIT=0.5}
                 After 30 long days, you, great farmlord, have succeeded!
                 {WAIT=0.5}
@@ -150,26 +127,6 @@ public class EndCreditsDisplay extends UIComponent {
                 
                 {COLOR=green}Congrats You Win!!!.{WAIT=1}
             """;
-        } else {
-            credits = """ 
-                {WAIT=0.5}
-                Despite your best efforts, Alpha Centauri remains a wasteland.
-                
-                {WAIT}
-                As your oxygen supply dwindles, so does humanities hope for survival.
-                {WAIT=0.5}
-                
-                You gave it your all, but in the end... {WAIT=1} it wasn't enough.
-                {WAIT}
-                
-                This is the end of the human race.
-                {WAIT}
-                
-                {COLOR=red}You're a loser!.
-                {WAIT=1}
-            """;
-        }
-        return credits;
     }
 
     /**
@@ -191,38 +148,22 @@ public class EndCreditsDisplay extends UIComponent {
 
     @Override
     public void update() {
-        // This movement logic is triggered on every frame, until the middle of the planet hits its target position
-        // on screen
-        if (planet.getY(Align.center) >= storyLabel.getY(Align.top) + planetToTextPadding) {
-            planet.setY(planet.getY() - spaceSpeed); // Move the planet
-            background.setY(background.getY() - spaceSpeed); // Move the background
-        }
+        // Calculate the new position for the storyLabel
+        float newY = storyLabel.getY() + spaceSpeed * Gdx.graphics.getDeltaTime();
 
-        // Resize the planet to the new screen size, maintaining aspect ratio
-        float planetWidth = (float) (Gdx.graphics.getWidth() * 0.1);
-        float planetHeight = planetWidth * (planet.getHeight() / planet.getWidth());
-        planet.setSize(planetWidth, planetHeight);
-
-        // re-center the planet
-        planet.setPosition((float)Gdx.graphics.getWidth()/2, planet.getY(Align.center), Align.center);
-
-        // adjust the speed of movement based on screen size and current fps to ensure planet
-        // hits position at the right time
-        float calculatedSpeed = (planet.getY(Align.center) - storyLabel.getY(Align.top) + planetToTextPadding) /
-                (textAnimationDuration * (float)Gdx.graphics.getFramesPerSecond());
-
-        // The universal speed limit must be enforced
-        if (calculatedSpeed > 1.5f) {
-            spaceSpeed = 1.5f;
+        // Check if the storyLabel has scrolled past the top of the screen
+        if (newY >= Gdx.graphics.getHeight()) {
+            // You can hide the text or do something when it exits the screen
+            storyLabel.setVisible(false);
         } else {
-            spaceSpeed = calculatedSpeed;
+            // Update the Y position of the storyLabel
+            storyLabel.setPosition(storyLabel.getX(), newY);
         }
 
-        logger.debug(String.format("Space Speed: %s", spaceSpeed));
+        // Rest of your update logic...
 
-        //stage.act(ServiceLocator.getTimeSource().getDeltaTime());
-
-        //this.returnButton.setVisible(true);
+        // Update the stage
+        stage.act(Gdx.graphics.getDeltaTime());
     }
 
     @Override
