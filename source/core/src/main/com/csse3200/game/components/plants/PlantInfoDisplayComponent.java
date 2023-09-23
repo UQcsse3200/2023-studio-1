8fff27ace4f60377f65d799e2d8f2095a5432e43
Tom <t.edwards1@uqconnect.edu.au>
2023-09-23 14:54:50 +1000

    added javadocs to the PlantInfoDisplayComponent class

--- source/core/src/main/com/csse3200/game/components/plants/PlantInfoDisplayComponent.java
+++ source/core/src/main/com/csse3200/game/components/plants/PlantInfoDisplayComponent.java
@@ -10,14 +10,23 @@ import com.csse3200.game.ui.UIComponent;
 
 public class PlantInfoDisplayComponent extends UIComponent {
 
+    /**
+     * The window used to display the plant information.
+     */
     private Window window;
+
+    /**
+     * The label for the text displaying the plant information.
+     */
     private Label label;
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void create() {
         super.create();
-        addActors();
-        ServiceLocator.getPlantInfoService().getEvents().addListener("showPlantInfo", this::testPlant);
+        ServiceLocator.getPlantInfoService().getEvents().addListener("showPlantInfo", this::showPlantInfo);
         ServiceLocator.getPlantInfoService().getEvents().addListener("clearPlantInfo", this::clearInfo);
         createWindow();
         label = new Label("Hover your mouse \nover a plant", skin);
@@ -28,10 +37,9 @@ public class PlantInfoDisplayComponent extends UIComponent {
 
     }
 
-    private void addActors() {
-        createWindow();
-    }
-
+    /**
+     * Create the window used to display the plant information.
+     */
     private void createWindow() {
         window = new Window("Plant information", skin);
         window.setVisible(true);
@@ -39,23 +47,28 @@ public class PlantInfoDisplayComponent extends UIComponent {
         window.padBottom(10f);
         window.setPosition(20f, 20f);
         window.setMovable(false);
-
         stage.addActor(window);
     }
 
-    public void testPlant(String test) {
+    /**
+     * Display the string of plant information in the window. This information is
+     * provided by the plant component and is already formatted. This occurs when the
+     * player hovers their mouse cursor over a plant.
+     * @param plantInfo - Information about the current state of the plant.
+     */
+    public void showPlantInfo(String plantInfo) {
         window.reset();
         createWindow();
-
-        label = new Label(test, skin);
+        label = new Label(plantInfo, skin);
         label.setFontScale(1.5f);
         label.setColor(Color.BROWN);
-
         window.add(label);
         stage.addActor(window);
-
     }
 
+    /**
+     * Clears the window of any plant information.
+     */
     public void clearInfo() {
         createWindow();
         label = new Label("Hover your mouse \nover a plant", skin);
@@ -65,12 +78,12 @@ public class PlantInfoDisplayComponent extends UIComponent {
         stage.addActor(window);
     }
 
-
-
-
-
+    /**
+     * Has no use in this component but is required by the UIComponent class.
+     * @param batch Batch to render to.
+     */
     @Override
     protected void draw(SpriteBatch batch) {
-
+        // No use but required.
     }
 }
