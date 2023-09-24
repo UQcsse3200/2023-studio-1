77b76739e678b89d99b3e503c8695022a9311d89
RileyWhitty <riley.whitlock@uq.net.au>
2023-09-24 11:41:02 +1000

    Created new Cutscene class and added base methods with no code body but comments on what each method should do

---
+++ source/core/src/main/com/csse3200/game/missions/cutscenes/Cutscene.java
@@ -0,0 +1,63 @@
+package com.csse3200.game.missions.cutscenes;
+public class Cutscene {
+
+    // creates an instance of the cutscene class and assigns all variables as they need to be assigned - DOES NOT SPAWN THE ACTUAL CUTSCENE
+    public Cutscene() {
+
+    }
+
+    // Creates the whole cutscene - to call other methods below this method
+    public void spawnCutscene() {
+
+    }
+
+    // pauses the game
+    public void pauseGame() {
+
+    }
+
+    // Spawns the sprites/entities that will be on the left/right side of the screen
+    public void spawnSprites() {
+
+    }
+
+    // Spawn the dialogue box and populate it with text
+    public void spawnDialogueBox () {
+
+    }
+
+    // animate the sprites/entities on the left/right sides of the screen
+    public void animateSprites() {
+
+    }
+
+    // Method to handle when the exit button is pressed. Will likely need some kind of event handler
+    public void handleExitButtonPress() {
+
+    }
+
+    // Ends the cutscene, to call the other methods below
+    public void endCutscene() {
+
+    }
+
+    // deletes/clears the sprites/entities spawned in
+    public void clearSprites() {
+
+    }
+
+    // deletes/clears the dialogue box
+    public void clearDialogueBox() {
+
+    }
+
+    // deletes/clears the exit button
+    public void clearExitButton() {
+
+    }
+
+    // unpauses the game
+    public void unPauseGame() {
+
+    }
+}
