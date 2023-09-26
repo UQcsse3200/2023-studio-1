06b64459e53a5bf4e4e9ff3ff6615826e7a28e72
Shristi Gupta <guptashristi1408@gmail.com>
2023-09-26 22:55:36 +1000

    #325 Added the implementation to render multiple particle effects on the screen.

--- source/core/src/main/com/csse3200/game/areas/weather/AcidShower.java
+++ source/core/src/main/com/csse3200/game/areas/weather/AcidShower.java
@@ -4,29 +4,45 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.ParticleEffect;
+import com.badlogic.gdx.graphics.g2d.ParticleEffectPool;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Pool;
 
 public class AcidShower implements Screen {
     private ParticleEffect acidShowerParticleEffect;
+    private ParticleEffectPool acidShowerParticleEffectPool;
     private SpriteBatch spriteBatch;
+    private Array<ParticleEffectPool.PooledEffect> pooledEffectsArray;
 
     @Override
     public void show() {
         acidShowerParticleEffect = new ParticleEffect();
-        spriteBatch = new SpriteBatch();
         acidShowerParticleEffect.load(Gdx.files.internal("particle-effects/acidrain"), Gdx.files.internal("images"));
-        acidShowerParticleEffect.setPosition((float) Gdx.graphics.getWidth()/2, (float) Gdx.graphics.getHeight()/2);
-        acidShowerParticleEffect.start();
+        acidShowerParticleEffect.setEmittersCleanUpBlendFunction(false);
+        acidShowerParticleEffectPool = new ParticleEffectPool(acidShowerParticleEffect, 1, 2);
+        spriteBatch = new SpriteBatch();
+        pooledEffectsArray = new Array<>();
+
+        addMultipleParticleEffects();
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
         spriteBatch.begin();
-        acidShowerParticleEffect.draw(spriteBatch, delta);
+        for(int i = pooledEffectsArray.size - 1; i >= 0; i--) {
+            ParticleEffectPool.PooledEffect pooledEffect = pooledEffectsArray.get(i);
+            pooledEffect.draw(spriteBatch, delta);
+            if(pooledEffect.isComplete()) {
+                pooledEffect.free();
+                pooledEffectsArray.removeIndex(i);
+            }
+        }
         spriteBatch.end();
+        addMultipleParticleEffects();
     }
 
     @Override
@@ -51,6 +67,22 @@ public class AcidShower implements Screen {
 
     @Override
     public void dispose() {
+        for (ParticleEffectPool.PooledEffect pooledEffect : pooledEffectsArray) {
+            pooledEffect.free();
+        }
+        pooledEffectsArray.clear();
+        spriteBatch.dispose();
+        acidShowerParticleEffect.dispose();
+    }
 
+    private void addMultipleParticleEffects() {
+        while (pooledEffectsArray.size < 10) {
+            ParticleEffectPool.PooledEffect pooledEffect = acidShowerParticleEffectPool.obtain();
+            float randomX = MathUtils.random(0, Gdx.graphics.getWidth());
+            float randomY = MathUtils.random(0, Gdx.graphics.getHeight());
+            pooledEffect.setPosition(randomX, randomY);
+            pooledEffectsArray.add(pooledEffect);
+            pooledEffect.start();
+        }
     }
 }
