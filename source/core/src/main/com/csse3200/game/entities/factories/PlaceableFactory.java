92aa9ce95b9c939a1938867aba65b6b4ca7c0f3f
Japamaja <Jacksontrenerry01@gmail.com>
2023-09-29 09:15:58 +1000

    #292 changes the destory / shovel method in ItemActions.

--- source/core/src/main/com/csse3200/game/entities/factories/PlaceableFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/PlaceableFactory.java
@@ -1,7 +1,9 @@
 package com.csse3200.game.entities.factories;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.csse3200.game.areas.terrain.TerrainTile;
 import com.csse3200.game.components.placeables.PlaceableCategory;
 import com.csse3200.game.components.placeables.SprinklerComponent;
 import com.csse3200.game.components.placeables.FenceComponent;
@@ -15,6 +17,8 @@ import com.csse3200.game.physics.components.ColliderComponent;
 import com.csse3200.game.physics.components.HitboxComponent;
 import com.csse3200.game.physics.components.PhysicsComponent;
 import com.csse3200.game.rendering.DynamicTextureRenderComponent;
+import com.csse3200.game.services.FactoryService;
+import com.csse3200.game.services.ServiceLocator;
 
 public class PlaceableFactory {
 
@@ -29,7 +33,27 @@ public class PlaceableFactory {
                 .addComponent(new PhysicsComponent().setBodyType(BodyDef.BodyType.StaticBody))
                 .addComponent(new HitboxComponent())
                 .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+        placeable.getEvents().addListener("destroy", PlaceableFactory::destoryPlaceable);
         return placeable;
+    }
+
+    private void destoryPlaceable(TerrainTile tile) {
+        Entity placedItem = tile.getPlaceable();
+        // Check if the placeable is a chest and if there is items in that chest
+        InventoryComponent chestInventory = placedItem.getComponent(InventoryComponent.class);
+        if (chestInventory != null){
+            if (chestInventory.getInventory().size() >= 1) {
+                return;
+            }
+        }
+        // Removes references from the tile to the placeable and set it as unoccupied
+        tile.removeCropTile();
+        // Spawn in the item that corresponds to the removed placeable
+        Entity droppedItem = FactoryService.getItemFactories().get(placedItem.getType().toString()).get();
+        ServiceLocator.getGameArea().spawnEntity(droppedItem);
+        droppedItem.setPosition(placedItem.getPosition());
+        // Remove the placeable from the game area
+        ServiceLocator.getGameArea().removeEntity(placedItem);
     }
 
     /**
