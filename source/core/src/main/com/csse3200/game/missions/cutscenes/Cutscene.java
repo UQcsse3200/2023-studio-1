862bad49e7d38b32aaf453e9f04c896847fb334d
nathaniel chan <nathaniel.chan@uqconnect.edu.au>
2023-09-29 12:23:52 +1000

    added spawning of continue button

--- source/core/src/main/com/csse3200/game/missions/cutscenes/Cutscene.java
+++ source/core/src/main/com/csse3200/game/missions/cutscenes/Cutscene.java
@@ -1,13 +1,21 @@
 package com.csse3200.game.missions.cutscenes;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.csse3200.game.screens.MainGameScreen;
 import com.csse3200.game.screens.MainGameScreen.*;
 import com.csse3200.game.services.ServiceLocator;
 import net.dermetfan.gdx.CutsceneManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.csse3200.game.ui.UIComponent;
 
-public class Cutscene {
+public class Cutscene extends UIComponent {
     private static final Logger logger = LoggerFactory.getLogger(Cutscene.class);
+
+    private Table table = new Table();
 
     // creates an instance of the cutscene class and assigns all variables as they need to be assigned - DOES NOT SPAWN THE ACTUAL CUTSCENE
     public Cutscene() {
@@ -34,34 +42,30 @@ public class Cutscene {
 
     }
 
+    public void spawnContinueButton() {
+        TextButton continueBtn = new TextButton("Continue", skin);
+        continueBtn.addListener(
+                new ChangeListener() {
+                    @Override
+                    public void changed(ChangeEvent event, Actor actor) {
+                        logger.debug("Continue button clicked");
+                        // add a line to trigger event when this btn is clicked
+                    }
+                });
+
+        table.add(continueBtn).padTop(40f); // change 40f to whatever is needed
+        table.row();
+    }
+
     // animate the sprites/entities on the left/right sides of the screen
     public void animateSprites() {
 
     }
 
-    // Method to handle when the exit button is pressed. Will likely need some kind of event handler
-    public void handleExitButtonPress() {
-
-    }
-
-    // Ends the cutscene, to call the other methods below
+    // Ends the cutscene
     public void endCutscene() {
-
-    }
-
-    // deletes/clears the sprites/entities spawned in
-    public void clearSprites() {
-
-    }
-
-    // deletes/clears the dialogue box
-    public void clearDialogueBox() {
-
-    }
-
-    // deletes/clears the exit button
-    public void clearExitButton() {
-
+        table.clear();
+        super.dispose();
     }
 
     // unpauses the game
@@ -69,5 +73,10 @@ public class Cutscene {
         logger.debug("Setting paused state to: 1");
         // 1 is for delta time to run in normal speed
         ServiceLocator.getTimeSource().setTimeScale(1);
+    }
+
+    @Override
+    public void draw(SpriteBatch batch) {
+        // handled by the stage
     }
 }
