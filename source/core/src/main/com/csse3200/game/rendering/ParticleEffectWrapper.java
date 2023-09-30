c62b3f2ee96cddf5d45c69826dd77d4830bad89f
Lucas Hicks <lucasjhicks1@gmail.com>
2023-09-30 16:58:06 +1000

    #442 Adds javadocs to the particle effect wrapper.

--- source/core/src/main/com/csse3200/game/rendering/ParticleEffectWrapper.java
+++ source/core/src/main/com/csse3200/game/rendering/ParticleEffectWrapper.java
@@ -1,27 +1,60 @@
 package com.csse3200.game.rendering;
 
-import com.badlogic.gdx.graphics.g2d.ParticleEffectPool;
+import com.badlogic.gdx.graphics.g2d.ParticleEffectPool.PooledEffect;
 
+/**
+ * Wrapper for the particle effect so that pooled effects can have a type and a category
+ */
 public class ParticleEffectWrapper {
-	private final ParticleEffectPool.PooledEffect pooledEffect;
+
+	/**
+	 * Pooled particle effect that is grabbed from the pool
+	 */
+	private final PooledEffect effect;
+
+	/**
+	 * Category of the particle effect (eg: Weather event)
+	 */
 	private final String category;
+
+	/**
+	 * Type of particle effect (eg: Acid Shower)
+	 */
 	private final String type;
 
 
-	public ParticleEffectWrapper(ParticleEffectPool.PooledEffect effect, String category, String type) {
-		this.pooledEffect = effect;
+	/**
+	 * Wraps a pooled particle effect with a category and a type
+	 * @param effect pooled effect
+	 * @param category category of the particle effect
+	 * @param type type of particle effect
+	 */
+	public ParticleEffectWrapper(PooledEffect effect, String category, String type) {
+		this.effect = effect;
 		this.category = category;
 		this.type = type;
 	}
 
-	public ParticleEffectPool.PooledEffect getPooledEffect() {
-		return pooledEffect;
+	/**
+	 * Gets the pooled effect that is contained in the wrapper
+	 * @return pooled effect
+	 */
+	public PooledEffect getPooledEffect() {
+		return effect;
 	}
 
+	/**
+	 * Gets the wrappers category
+	 * @return category of the wrapper
+	 */
 	public String getCategory() {
 		return category;
 	}
 
+	/**
+	 * Gets the type of particle effect in the wrapper
+	 * @return type of particle effect
+	 */
 	public String getType() {
 		return type;
 	}
