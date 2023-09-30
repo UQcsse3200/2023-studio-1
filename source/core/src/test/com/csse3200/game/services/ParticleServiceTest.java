fe7311043f0fbec8617eb1618316effe14be2b32
Lucas Hicks <lucasjhicks1@gmail.com>
2023-09-30 17:21:28 +1000

    #441 Adds before and after methods to test class for cleanliness of service locator.

--- source/core/src/test/com/csse3200/game/services/ParticleServiceTest.java
+++ source/core/src/test/com/csse3200/game/services/ParticleServiceTest.java
@@ -1,5 +1,8 @@
 package com.csse3200.game.services;
 
+import com.badlogic.gdx.graphics.g2d.ParticleEmitter;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -10,4 +13,18 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ParticleServiceTest {
+
+    ParticleService particleService;
+
+    @BeforeEach
+    public void setUp() {
+        ServiceLocator.clear();
+        particleService = new ParticleService();
+        ServiceLocator.registerParticleService(particleService);
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        ServiceLocator.clear();
+    }
 }
