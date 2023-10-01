11b9e5bc4355ef00b67e2dc591e904b50b359037
Gil Wise <gilwise245@gmail.com>
2023-10-01 23:41:02 +1000

    Cleaned up play() functionality and prepared for removal of irrelevant MusicService methods.

--- source/core/src/main/com/csse3200/game/services/sound/BackgroundMusicService.java
+++ source/core/src/main/com/csse3200/game/services/sound/BackgroundMusicService.java
@@ -2,10 +2,8 @@ package com.csse3200.game.services.sound;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
-import com.csse3200.game.events.EventHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.csse3200.game.events.EventHandler;
 
 import java.util.*;
 
@@ -16,36 +14,38 @@ import java.util.*;
 public class BackgroundMusicService implements MusicService {
     
     private static final Logger logger = LoggerFactory.getLogger(BackgroundMusicService.class);
-    private final EventHandler eventHandler;
+    /** The maximum number of tracks loadable at once. */
     private static final int MAX_TRACKS = 10;
     
-    /**
-     * An array list of up to 10 background music SoundFiles.
-     */
+    /** An array list of up to 10 background music SoundFiles. */
     private ArrayList<BackgroundSoundFile> tracks;
     
+    /** A map of sound files to their corresponding Music instances. */
     private Map<BackgroundSoundFile, Music> loadedMusic;
+    
+    /** A map of music types to a list of the Music instances of that type. */
     private Map<BackgroundMusicType, ArrayList<Music>> categorisedMusic;
     
-    /**
-     * The number of tracks loaded at a given time.
-     */
+    /** The number of tracks loaded at a given time. */
     private int numLoaded;
     
-    /**
-     * A flag that controls whether background music is played.
-     */
+    /** A flag that controls whether background music has volume. */
     private boolean muteStatus;
     
-    /**
-     * The most recently played/playing track key.
-     */
+    /** A flag to determine whether background music is paused. */
+    private boolean pauseStatus;
+    
+    /** Flag to determine whether/when the next song is to be played. */
+    private boolean keepPlaying;
+    
+    /** The most recently played/playing SoundFile key. */
     private BackgroundSoundFile currentlyActive;
-
+    
+    /** The currently active Music instance. */
+    private Music currentMusic;
+    
     public BackgroundMusicService() {
         logger.debug("Initialising BackgroundMusicService");
-        eventHandler = new EventHandler();
-        //TODO: Add listener to "musicEnded" event through service locator which calls play()
         this.tracks = new ArrayList<>();
         this.loadedMusic = new HashMap<>();
         this.categorisedMusic = new HashMap<>();
@@ -56,31 +56,51 @@ public class BackgroundMusicService implements MusicService {
         }
         numLoaded = 0;
         this.muteStatus = false;
-        currentlyActive = null;
+        this.pauseStatus = false;
+        this.keepPlaying = true;
+        this.currentlyActive = null;
+        this.currentMusic = null;
     }
     
+    /**
+     * Begins playing loaded Music instances of the provided type. Will continuously play
+     * songs at random of the given type until stop() is called. After which play will need to
+     * be called again to resume playback.
+     * @param type The type of background music to be played.
+     * @throws IllegalStateException If there are no available Music instances of the type given.
+     */
     public void play(BackgroundMusicType type) throws IllegalStateException {
         this.stopCurrentlyPlaying();
+        this.keepPlaying = true;
         Random rand = new Random();
         if (categorisedMusic.get(type).isEmpty()) {
             throw new IllegalStateException("No tracks loaded of type " + type);
         } else {
-            while (true) {
-                Music music = categorisedMusic.get(type)
-                        .get(rand.nextInt(categorisedMusic.get(type).size()));
-                this.setupCompletionListener(music);
-                if (this.isMuted()) {
-                    music.setVolume(0.0f);
-                } else {
-                    music.setVolume(1.0f);
+            while (keepPlaying) { // While not stopped.
+                if (currentMusic == null || (!currentMusic.isPlaying() && !pauseStatus)) {
+                    // Select a random loaded song of the given type.
+                    currentMusic = categorisedMusic.get(type)
+                            .get(rand.nextInt(categorisedMusic.get(type).size()));
+                    // Set the completion listener to clean up after end of playback.
+                    setupCompletionListener(currentMusic);
+                    if (this.isMuted()) { // Apply mute status
+                        currentMusic.setVolume(0.0f);
+                    } else {
+                        currentMusic.setVolume(1.0f);
+                    }
+                    currentMusic.play(); // Play the music.
                 }
-                music.play();
-                //TODO set currently active to this music instance's BackgroundSoundFile
-                
             }
         }
     }
-
+    // TODO - UNNECESSARY FUNCTION
+    /**
+     *
+     * @param sound - An enum value that implements the SoundFile interface
+     * @param looping - A flag to control if the sound loops
+     * @return
+     * @throws InvalidSoundFileException
+     */
     @Override
     public long play(SoundFile sound, boolean looping) throws InvalidSoundFileException {
         logger.debug("Running checks on background music file.");
@@ -103,12 +123,14 @@ public class BackgroundMusicService implements MusicService {
         }
         return 0; // Play ID not needed for background music
     }
-
+    
+    // TODO - UNNECESSARY FUNCTION
     @Override
     public long play(SoundFile sound) throws InvalidSoundFileException {
         return this.play(sound, false);
     }
     
+    // TODO - UNNECESSARY FUNCTION
     /**
      * Attempts to pause a specific track. This is more important for Effects. Use pause()
      * to pause whichever track is currently active.
@@ -117,8 +139,9 @@ public class BackgroundMusicService implements MusicService {
      */
     @Override
     public void pause(SoundFile sound) throws InvalidSoundFileException {
-        if (sound == currentlyActive) {
+        if (sound.equals(currentlyActive)) {
             loadedMusic.get(sound).pause();
+            pauseStatus = true;
         } else {
             throw new InvalidSoundFileException("Given parameter is not currently playing");
         }
@@ -129,13 +152,20 @@ public class BackgroundMusicService implements MusicService {
      */
     public void pause() {
         loadedMusic.get(currentlyActive).pause();
+        pauseStatus = true;
     }
-
-    //TODO
+    
+    public void unPause() {
+        loadedMusic.get(currentlyActive).play();
+        pauseStatus = false;
+    }
+    
     @Override
     public void stop(SoundFile sound) throws InvalidSoundFileException {
         if (sound != currentlyActive) {
-        
+            throw new InvalidSoundFileException("Attempting to stop sound that is not playing.");
+        } else {
+            stopCurrentlyPlaying();
         }
     }
     
@@ -145,6 +175,8 @@ public class BackgroundMusicService implements MusicService {
     private void stopCurrentlyPlaying() {
         if (currentlyActive != null) {
             loadedMusic.get(currentlyActive).stop();
+            keepPlaying = false;
+            currentMusic = null;
         }
     }
 
@@ -198,22 +230,36 @@ public class BackgroundMusicService implements MusicService {
         }
     }
     
+    /**
+     * Sets up the completion listener for a Music instance to end playback, allowing the
+     * next track to play.
+     * @param music the Music instance to add the listener to.
+     */
     private void setupCompletionListener(Music music) {
         music.setOnCompletionListener(new Music.OnCompletionListener() {
             @Override
             public void onCompletion(Music music) {
                 music.stop();
-                //eventHandler.trigger("musicEnded");
             }
         });
     }
-
+    
+    /**
+     * Disposes of all loaded Music instances.
+     * Resets internally stored sound file maps.
+     * To resume playback after calling this, loadSounds() must be called again.
+     */
     @Override
     public void dispose() {
         logger.debug("Disposing loaded Music instances");
+        this.currentMusic = null;
+        this.currentlyActive = null;
         for (BackgroundSoundFile track : tracks) {
             loadedMusic.get(track).dispose();
         }
+        this.tracks = new ArrayList<>();
+        this.loadedMusic = new HashMap<>();
+        this.categorisedMusic = new HashMap<>();
     }
     
     
