5cd9de558b2881e6ded69babc46e3d2bb68d752c
Braden <b.bowie@uqconnect.edu.au>
2023-10-01 23:54:40 +1100

    Added Shooting test and Finished Sword Test

--- source/core/src/test/com/csse3200/game/components/player/PlayerActionsTest.java
+++ source/core/src/test/com/csse3200/game/components/player/PlayerActionsTest.java
@@ -1,15 +1,20 @@
 package com.csse3200.game.components.player;
 
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.math.Vector2;
 import com.csse3200.game.ai.tasks.AITaskComponent;
 import com.csse3200.game.areas.GameArea;
 import com.csse3200.game.areas.SpaceGameArea;
 import com.csse3200.game.areas.terrain.TerrainFactory;
 import com.csse3200.game.components.CameraComponent;
+import com.csse3200.game.components.CombatStatsComponent;
+import com.csse3200.game.components.Component;
+import com.csse3200.game.components.combat.ProjectileComponent;
 import com.csse3200.game.components.tasks.FollowTask;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.entities.EntityService;
 import com.csse3200.game.entities.factories.PlayerFactory;
+import com.csse3200.game.entities.factories.ProjectileFactory;
 import com.csse3200.game.entities.factories.RenderFactory;
 import com.csse3200.game.input.InputService;
 import com.csse3200.game.missions.MissionManager;
@@ -21,13 +26,21 @@ import com.csse3200.game.rendering.Renderer;
 import com.csse3200.game.services.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 public class PlayerActionsTest {
+
+    private List<Entity> areaEntities;
+
+    private Entity player;
     @BeforeEach
     void beforeEach() {
         // Mock rendering, physics, game time
@@ -38,7 +51,7 @@ public class PlayerActionsTest {
         ServiceLocator.registerPhysicsService(physicsService);
 
         ServiceLocator.registerInputService(new InputService());
-        ServiceLocator.registerResourceService(new ResourceService());
+        ServiceLocator.registerResourceService(mock(ResourceService.class));
 
         ServiceLocator.registerEntityService(new EntityService());
         ServiceLocator.registerRenderService(new RenderService());
@@ -46,13 +59,39 @@ public class PlayerActionsTest {
         ServiceLocator.registerPlanetOxygenService(new PlanetOxygenService());
 
         ServiceLocator.registerGameArea(mock(GameArea.class));
+
+        areaEntities = new ArrayList<>();
+        player = new Entity();
+        player.addComponent(new PlayerActions());
+        player.setPosition(0,0);
+        Entity chicken = new Entity();
+        chicken.addComponent(new CombatStatsComponent(10, 0));
+        chicken.setPosition(1,1);
+        areaEntities.add(player);
+        areaEntities.add(chicken);
+
+        when(ServiceLocator.getGameArea().getAreaEntities()).thenReturn(areaEntities);
+        when(ServiceLocator.getResourceService().getAsset(any(), any())).thenReturn(mock(Sound.class));
+        when(ServiceLocator.getCameraComponent().screenPositionToWorldPosition(any())).thenReturn(new Vector2(2, 2));
     }
 
     @Test
     void playerShouldAttackNPC() {
-        Entity target = new Entity();
-        target.addComponent(new PlayerActions());
-        PlayerActions playerActions = target.getComponent(PlayerActions.class);
+        PlayerActions playerActions = player.getComponent(PlayerActions.class);
+        playerActions.attack(new Vector2(2,2));
+        assertEquals(5, areaEntities.get(1).getComponent(CombatStatsComponent.class).getHealth());
+    }
+
+    @Test
+    void playerShouldShootNPC() {
+        //doNothing().when(ProjectileFactory.createPlayerProjectile());
+        //when(ProjectileFactory.createPlayerProjectile()).thenReturn(mock(Entity.class));
+        //mockStatic(ProjectileFactory.class);
+        //doReturn(new Entity()).when(ProjectileFactory.createPlayerProjectile());
+        //when((any(Entity.class).getComponent(ProjectileComponent.class))).thenReturn(mock(ProjectileComponent.class));
+        PlayerActions playerActions = player.getComponent(PlayerActions.class);
+        //playerActions.shoot(new Vector2(2,2));
+        //assertEquals(3, areaEntities.size());
     }
 
 }
