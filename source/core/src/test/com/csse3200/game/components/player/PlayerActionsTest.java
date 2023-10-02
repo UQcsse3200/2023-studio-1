a0c50a06d27fbeab1c0ae675e7fb352a4919db15
Braden <b.bowie@uqconnect.edu.au>
2023-10-03 00:26:59 +1100

    Fixed Minor Code Smells in PlayerActionsTest

--- source/core/src/test/com/csse3200/game/components/player/PlayerActionsTest.java
+++ source/core/src/test/com/csse3200/game/components/player/PlayerActionsTest.java
@@ -36,7 +36,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-public class PlayerActionsTest {
+class PlayerActionsTest {
 
     private List<Entity> areaEntities;
 
