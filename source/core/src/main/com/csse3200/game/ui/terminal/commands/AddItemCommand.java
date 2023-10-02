c15352de96d099d6f34c3f7216e985bc3bc20e6a
Lucas Hicks <lucasjhicks1@gmail.com>
2023-10-02 14:47:24 +1000

    Adds to the addItem command.

--- source/core/src/main/com/csse3200/game/ui/terminal/commands/AddItemCommand.java
+++ source/core/src/main/com/csse3200/game/ui/terminal/commands/AddItemCommand.java
@@ -38,6 +38,7 @@ public class AddItemCommand implements Command {
 			case "can" -> item = ItemFactory.createWateringcan();
 			case "scythe" -> item = ItemFactory.createScythe();
 			case "sprinkler" -> item = ItemFactory.createSprinklerItem();
+			case "pump" -> item = ItemFactory.createPumpItem();
 			case "fence" -> item = ItemFactory.createFenceItem();
 			case "gate" -> item = ItemFactory.createGateItem();
 			case "chest" -> item = ItemFactory.createChestItem();
