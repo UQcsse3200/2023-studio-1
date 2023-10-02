b4686f5a7c43012f8b922cd115038295f52b86b8
Donghyug David Jeong <donghyug.jeong@uqconnect.edu.au>
2023-10-02 15:52:40 +1000

    #387 ShipRepairQuestTest - testIsCompleted()

--- source/core/src/test/com/csse3200/game/missions/quests/ShipRepairQuestTest.java
+++ source/core/src/test/com/csse3200/game/missions/quests/ShipRepairQuestTest.java
@@ -5,17 +5,20 @@ import com.csse3200.game.missions.rewards.Reward;
 import com.csse3200.game.services.GameTime;
 import com.csse3200.game.services.ServiceLocator;
 import com.csse3200.game.services.TimeService;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
 class ShipRepairQuestTest {
     private ShipRepairQuest SRQuest1, SRQuest2, SRQuest3, SRQuest4, SRQuest5, SRQuest6, SRQuest7, SRQuest8;
-    private Reward r1, r2, r3, r4, r5, r6, r7;
+    private Reward r1;
 
     @BeforeEach
     public void init() {
@@ -23,17 +26,44 @@ class ShipRepairQuestTest {
         ServiceLocator.registerTimeService(new TimeService());
         ServiceLocator.registerMissionManager(new MissionManager());
 
-        r1 = mock(Reward.class);
-        r2 = mock(Reward.class);
-        r3 = mock(Reward.class);W
-        r4 = mock(Reward.class);
-        r5 = mock(Reward.class);
-        r6 = mock(Reward.class);
-        r7 = mock(Reward.class);
-
         SRQuest1 = new ShipRepairQuest("Ship Repair Quest 1", r1, 10);
-        SRQuest1 = new ShipRepairQuest("Ship Repair Quest 1", r1, 0);
-        SRQuest1 = new ShipRepairQuest("Ship Repair Quest 1", r1, 10, 10);
+        SRQuest2 = new ShipRepairQuest("Ship Repair Quest 2", r1, -1);
+        SRQuest3 = new ShipRepairQuest("Ship Repair Quest 3", r1, 5);
+        SRQuest4 = new ShipRepairQuest("Ship Repair Quest 4", r1, 10, 10);
+        SRQuest5 = new ShipRepairQuest("Ship Repair Quest 5", r1, 10, -1);
 
     }
+    @AfterEach
+    public void reset() {
+        ServiceLocator.clear();
+    }
+
+    @Test
+    public void testIsCompleted() {
+        SRQuest1.registerMission(ServiceLocator.getMissionManager().getEvents());
+        SRQuest2.registerMission(ServiceLocator.getMissionManager().getEvents());
+        SRQuest3.registerMission(ServiceLocator.getMissionManager().getEvents());
+        SRQuest4.registerMission(ServiceLocator.getMissionManager().getEvents());
+        SRQuest5.registerMission(ServiceLocator.getMissionManager().getEvents());
+        for (int i = 0; i < 10; i++) {
+            assertFalse(SRQuest1.isCompleted());
+            assertTrue(SRQuest2.isCompleted());
+            if (i >= 5) {
+                assertTrue(SRQuest3.isCompleted());
+            } else {
+                assertFalse(SRQuest3.isCompleted());
+            }
+            assertFalse(SRQuest4.isCompleted());
+            assertTrue(SRQuest5.isCompleted());
+            ServiceLocator.getMissionManager().getEvents().trigger(MissionManager.MissionEvent.SHIP_PART_ADDED.name());
+        }
+        assertTrue(SRQuest1.isCompleted());
+        assertTrue(SRQuest2.isCompleted());
+        assertTrue(SRQuest3.isCompleted());
+        assertTrue(SRQuest4.isCompleted());
+        assertTrue(SRQuest5.isCompleted());
+    }
+
+
+
 }
