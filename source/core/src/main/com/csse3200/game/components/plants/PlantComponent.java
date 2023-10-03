a9005972b628763f6c8ea074d54374de2b549549
86LAK <lakshan392@gmail.com>
2023-10-03 22:49:00 +1000

    #528 removed unused import from PlantComponent

--- source/core/src/main/com/csse3200/game/components/plants/PlantComponent.java
+++ source/core/src/main/com/csse3200/game/components/plants/PlantComponent.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.function.Supplier;
 
-import com.badlogic.gdx.math.Vector2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
