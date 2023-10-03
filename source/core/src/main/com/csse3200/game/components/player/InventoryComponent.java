5ac0d208e820341e5bb18b9d97fd5f675ee70c71
masoo12 <a345cde@gmail.com>
2023-10-03 12:42:07 +1100

    Fixed double use bug.

--- source/core/src/main/com/csse3200/game/components/player/InventoryComponent.java
+++ source/core/src/main/com/csse3200/game/components/player/InventoryComponent.java
@@ -53,9 +53,9 @@ public class InventoryComponent extends Component {
     if (heldItem == null) {
       return false;
     }
-    if (heldItem.getComponent(ItemComponent.class).isPerishable()) {
+    /*if (heldItem.getComponent(ItemComponent.class).isPerishable()) {
       return removeItem(this.heldItem);
-    }
+    }*/
     return true;
   }
 
