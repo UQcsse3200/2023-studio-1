e8eaf1a191d03a2296f5f105506128d3231fe283
86LAK <lakshan392@gmail.com>
2023-10-03 22:28:43 +1000

    #528 added useful comments to solarsurge as to why start and stop effect is empty

--- source/core/src/main/com/csse3200/game/areas/weather/SolarSurgeEvent.java
+++ source/core/src/main/com/csse3200/game/areas/weather/SolarSurgeEvent.java
@@ -42,7 +42,7 @@ public class SolarSurgeEvent extends WeatherEvent {
      */
     @Override
     public void startEffect() {
-        // This method is empty because ngl i have no clue why but sonarcloud told me to put something here so TADA!!!
+        //  Handled elsewhere
     }
 
     /**
@@ -50,6 +50,6 @@ public class SolarSurgeEvent extends WeatherEvent {
      */
     @Override
     public void stopEffect() {
-        // This method is empty because ngl i have no clue why but sonarcloud told me to put something here so TADA!!!
+        // Handled elsewhere
     }
 }
