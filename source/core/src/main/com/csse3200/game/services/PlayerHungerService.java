2081268f785eb90513ae6633cfd0767e2d566b4c
Sanchit Jain <sanchitjain1632@gmail.com>
2023-10-04 13:26:45 +1000

    Removed unused lines

--- source/core/src/main/com/csse3200/game/services/PlayerHungerService.java
+++ source/core/src/main/com/csse3200/game/services/PlayerHungerService.java
@@ -5,8 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PlayerHungerService {
-    private static final Logger logger = LoggerFactory.getLogger(PlayerHungerService.class);
-
     private final EventHandler eventHandler;
 
     public PlayerHungerService() {
