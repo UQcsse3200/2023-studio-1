8c7bb7ff01ddbcfcdcfe94c5f2f3332181f1b6f7
Sanchit Jain <sanchitjain1632@gmail.com>
2023-10-04 13:24:34 +1000

    Removed unused lines and variables

--- source/core/src/main/com/csse3200/game/services/PlayerHungerService.java
+++ source/core/src/main/com/csse3200/game/services/PlayerHungerService.java
@@ -1,26 +1,20 @@
 package com.csse3200.game.services;
 
-import com.csse3200.game.entities.Entity;
-import com.csse3200.game.entities.EntityType;
 import com.csse3200.game.events.EventHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PlayerHungerService {
     private static final Logger logger = LoggerFactory.getLogger(PlayerHungerService.class);
-    private float hungerGoal;
-    private float hungerPresent;
-    private float delta;
+
     private final EventHandler eventHandler;
 
     public PlayerHungerService() {
         eventHandler = new EventHandler();
     }
 
-
     public EventHandler getEvents() {
         return eventHandler;
     }
-
 
 }
