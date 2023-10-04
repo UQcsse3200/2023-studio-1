ed33c471e43a7836ee8c23adc0e67c0d59b9002e
Codie <codie.chung@uqconnect.edu.au>
2023-10-04 12:25:09 +1000

    Fixed switch case in ProgressBar.java

--- source/core/src/main/com/csse3200/game/services/ProgressBar.java
+++ source/core/src/main/com/csse3200/game/services/ProgressBar.java
@@ -74,9 +74,9 @@ public class ProgressBar extends UIComponent {
         int day = ServiceLocator.getTimeService().getDay() - dayOffset;
 
         switch (this.act) {
-            case 1 -> progressBar = (day > 5) ? progressBarImagesAct1.get(0) : progressBarImagesAct1.get(day);
-            case 2 -> progressBar = (day > 10) ? progressBarImagesAct2.get(0) : progressBarImagesAct2.get(day);
-            case 3 -> progressBar = (day > 15) ? progressBarImagesAct3.get(0) : progressBarImagesAct3.get(day);
+            case 1 -> progressionbar = (day >= 5) ? progressBarImagesAct1.get(0) : progressBarImagesAct1.get(day);
+            case 2 -> progressionbar = (day >= 10) ? progressBarImagesAct2.get(0) : progressBarImagesAct2.get(day);
+            case 3 -> progressionbar = (day >= 15) ? progressBarImagesAct3.get(0) : progressBarImagesAct3.get(day);
         }
     }
 
