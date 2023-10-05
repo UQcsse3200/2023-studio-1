d783e1759a1847146daec1db3aeb803f2279c85a
Peter Lahanas <plahanas18@gmail.com>
2023-10-05 17:38:43 +1000

    553 added gate sound on interact

--- source/core/src/main/com/csse3200/game/services/sound/EffectSoundFile.java
+++ source/core/src/main/com/csse3200/game/services/sound/EffectSoundFile.java
@@ -3,6 +3,7 @@ package com.csse3200.game.services.sound;
 public enum EffectSoundFile implements SoundFile {
     // TODO: Add enum declarations for all effect sound tracks
     TRACTOR_HONK("sounds/car-horn-6408.mp3"),
+    GATE_INTERACT("sounds/gate-interact.wav"),
     IMPACT("sounds/Impact4.ogg");
 
     private final String filePath;
