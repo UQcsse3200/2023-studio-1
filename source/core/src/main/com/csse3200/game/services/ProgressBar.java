ad1622e645ed186530be9b6bcb49b0c9ff4bcc5b
Abhinavmohindersingh <141258868+Abhinavmohindersingh@users.noreply.github.com>
2023-10-07 11:20:23 +1000

    Variable undeclared progressbar to progression bar

--- source/core/src/main/com/csse3200/game/services/ProgressBar.java
+++ source/core/src/main/com/csse3200/game/services/ProgressBar.java
@@ -97,7 +97,7 @@ public class ProgressBar extends UIComponent {
         table.padTop(-30f).padLeft(190f);
 
 
-        group.addActor(progressBar);
+        group.addActor(progressionbar);
 
         table.add(group).size(200);
         stage.addActor(table);
@@ -110,6 +110,6 @@ public class ProgressBar extends UIComponent {
     @Override
     public void dispose() {
         super.dispose();
-        progressBar.remove();
+        group.addActor(progressionbar);
     }
 }
