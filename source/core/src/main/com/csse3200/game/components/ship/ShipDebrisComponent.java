8fb8bfbc50f21c169a1af30943e0f0bb27dd3dea
Olivia Ronda <o.ronda@uqconnect.edu.au>
2023-10-11 19:25:02 +1000

    #363 Chance to spawn ShipEater when destroying debris

--- source/core/src/main/com/csse3200/game/components/ship/ShipDebrisComponent.java
+++ source/core/src/main/com/csse3200/game/components/ship/ShipDebrisComponent.java
@@ -1,9 +1,13 @@
 package com.csse3200.game.components.ship;
 
+import com.csse3200.game.areas.GameArea;
 import com.csse3200.game.areas.terrain.TerrainTile;
 import com.csse3200.game.components.Component;
+import com.csse3200.game.entities.factories.NPCFactory;
 import com.csse3200.game.missions.MissionManager;
 import com.csse3200.game.services.ServiceLocator;
+
+import java.security.SecureRandom;
 
 /**
  * Handles Ship Debris events.
@@ -11,6 +15,7 @@ import com.csse3200.game.services.ServiceLocator;
  * Currently, triggers a DEBRIS_CLEARED event on the mission manager when destroyed.
  */
 public class ShipDebrisComponent extends Component {
+	SecureRandom random = new SecureRandom();
 
 	@Override
 	public void create() {
@@ -22,6 +27,11 @@ public class ShipDebrisComponent extends Component {
 	 * Trigger the mission manager's DEBRIS_CLEARED event then self destruct.
 	 */
 	void destroy(TerrainTile tile) {
+		if (random.nextInt(2) == 0) {
+			// unlucky, shipeater will spawn!
+			GameArea gameArea = ServiceLocator.getGameArea();
+			gameArea.spawnEntityAt(NPCFactory.createShipEater(), gameArea.getMap().vectorToTileCoordinates(entity.getCenterPosition()), true, true);
+		}
 		ServiceLocator.getMissionManager().getEvents().trigger(MissionManager.MissionEvent.DEBRIS_CLEARED.name());
 		if (tile != null) tile.removeOccupant();
 		entity.dispose();
