a057fe8583b3a6250f7893957504b6cd82145cc5
RileyWhitty <riley.whitlock@uq.net.au>
2023-10-14 10:08:47 +1000

    Made dialogue text speeds faster by default

--- source/core/src/main/com/csse3200/game/components/cutscenes/CutsceneDisplay.java
+++ source/core/src/main/com/csse3200/game/components/cutscenes/CutsceneDisplay.java
@@ -186,7 +186,7 @@ public class CutsceneDisplay extends UIComponent {
         TypingLabel dialogueLabel = new TypingLabel(this.dialogue, skin);
         dialogueLabel.setAlignment(Align.center);
         dialogueLabel.setWrap(true);
-        dialogueLabel.setDefaultToken("{COLOR=BLACK}");
+        dialogueLabel.setDefaultToken("{FAST}{COLOR=BLACK}");
 
         dialogueLabel.setTypingListener(new TypingAdapter() {
             @Override
