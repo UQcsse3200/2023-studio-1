ebcf88792b5596baef8428e9440581dbdd16f9a4
Japamaja <Jacksontrenerry01@gmail.com>
2023-10-14 19:51:45 +1000

    #663 Adds water quest to FactoryService

--- source/core/src/main/com/csse3200/game/services/FactoryService.java
+++ source/core/src/main/com/csse3200/game/services/FactoryService.java
@@ -117,6 +117,7 @@ public class FactoryService {
             new AbstractMap.SimpleEntry<String, Supplier<Quest>>(QuestFactory.FISHING_QUEST, QuestFactory::createFishingQuest),
             new AbstractMap.SimpleEntry<String, Supplier<Quest>>(QuestFactory.ANIMAL_REPEAT_QUEST, QuestFactory::createRecursiveAnimalQuest),
             new AbstractMap.SimpleEntry<String, Supplier<Quest>>(QuestFactory.PLANT_REPEAT_QUEST, QuestFactory::createRecursivePlantQuest),
+            new AbstractMap.SimpleEntry<String, Supplier<Quest>>(QuestFactory.WATER_REPEAT_QUEST, QuestFactory::createRecursiveWaterQuest),
             new AbstractMap.SimpleEntry<String, Supplier<Quest>>(QuestFactory.TRACTOR_GO_BRRRRRR, QuestFactory::createTractorQuest));
 
     public static Map<String, Function<CropTileComponent, Entity>> getPlantFactories() {
