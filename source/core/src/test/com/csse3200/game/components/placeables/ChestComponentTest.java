279ec4d21df8f06e8dd95084518c738cd1ff9b00
LiamAnd <s4800410@student.uq.edu.au>
2023-10-14 20:16:10 +1000

    Updated tests for Watering-can Component and created Tests for the Chest Component

---
+++ source/core/src/test/com/csse3200/game/components/placeables/ChestComponentTest.java
@@ -0,0 +1,30 @@
+package com.csse3200.game.components.placeables;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.junit.jupiter.api.Test;
+
+import com.csse3200.game.components.inventory.InventoryDisplay;
+import com.csse3200.game.components.player.InventoryComponent;
+import com.csse3200.game.extensions.GameExtension;
+import com.csse3200.game.entities.Entity;
+
+@ExtendWith(GameExtension.class)
+public class ChestComponentTest {
+    @Test
+    void toggleChest(){
+        Entity chest = new Entity();
+        chest.addComponent(new InventoryComponent())
+            .addComponent(new InventoryDisplay("refreshChest", "toggleChest", 30, 10, false))
+            .addComponent(new ChestComponent());
+
+        InventoryDisplay chestInv = chest.getComponent(InventoryDisplay.class);
+
+        assertFalse(chestInv.isOpen());     //chest is not opened
+        //trigger the open chest action
+        chest.getEvents().trigger("interact");
+        assertTrue(chestInv.isOpen());      //chest is open
+    }
+}
