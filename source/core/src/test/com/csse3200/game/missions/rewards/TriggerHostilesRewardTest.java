3cc4361451ec81a9826732de8cb0e0db500457f0
charliebkw <charlotte.birkinshaw@bigpond.com>
2023-10-16 00:03:07 +1000

    fix testing for the hostiles reward #598

--- source/core/src/test/com/csse3200/game/missions/rewards/TriggerHostilesRewardTest.java
+++ source/core/src/test/com/csse3200/game/missions/rewards/TriggerHostilesRewardTest.java
@@ -1,3 +1,81 @@
-package com.csse3200.game.missions.rewards;public class TriggerHostilesRewardTest {
-    // TODO remove or do something like why is this in main?????????????????
+package com.csse3200.game.missions.rewards;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.math.GridPoint2;
+import com.badlogic.gdx.math.Vector2;
+import com.csse3200.game.areas.GameArea;
+import com.csse3200.game.areas.SpaceGameArea;
+import com.csse3200.game.areas.terrain.GameMap;
+import com.csse3200.game.areas.terrain.TerrainFactory;
+import com.csse3200.game.areas.terrain.TerrainTile;
+import com.csse3200.game.areas.weather.ClimateController;
+import com.csse3200.game.components.CameraComponent;
+import com.csse3200.game.entities.EntitiesSpawner;
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.entities.EntitySpawner;
+import com.csse3200.game.entities.factories.NPCFactory;
+import com.csse3200.game.entities.factories.PlayerFactory;
+import com.csse3200.game.files.FileLoader;
+import com.csse3200.game.services.ServiceLocator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+public class TriggerHostilesRewardTest {
+
+    TriggerHostilesReward triggerHostilesReward;
+    SpaceGameArea spaceGameArea;
+
+    Entity player;
+    GameMap gameMap;
+    GridPoint2 playerPos;
+
+    EntitiesSpawner hostileSpawner;
+    TerrainTile terrainTile;
+
+
+
+    @BeforeEach
+    public void init() {
+        spaceGameArea = mock(SpaceGameArea.class);
+        ServiceLocator.registerGameArea(spaceGameArea);
+
+        player = mock(Entity.class);
+        when(spaceGameArea.getPlayer()).thenReturn(player);
+        when(spaceGameArea.getPlayer().getCenterPosition()).thenReturn(new Vector2(0,0));
+
+        gameMap = mock(GameMap.class);
+        when(spaceGameArea.getMap()).thenReturn(gameMap);
+        terrainTile = mock(TerrainTile.class);
+        when(gameMap.getTile(any(GridPoint2.class))).thenReturn(terrainTile);
+        when(terrainTile.isTraversable()).thenReturn(true);
+
+        playerPos = new GridPoint2(0,0);
+        when(spaceGameArea.getMap().vectorToTileCoordinates(spaceGameArea.getPlayer().getCenterPosition())).thenReturn(playerPos);
+
+        triggerHostilesReward = new TriggerHostilesReward(List.of(mock(Entity.class)));
+
+        // Create a mock EntitySpawner
+        EntitySpawner entitySpawner = mock(EntitySpawner.class);
+
+        // Create a List containing the mock EntitySpawner
+        List<EntitySpawner> entitySpawnerList = List.of(entitySpawner);
+
+        hostileSpawner = mock(EntitiesSpawner.class);
+
+        // Configure gameArea to return the hostileSpawner
+        when(spaceGameArea.getHostileSpawner()).thenReturn(hostileSpawner);
+    }
+
+    @Test
+    public void testCollect() {
+        triggerHostilesReward.collect();
+        SpaceGameArea gameArea = (SpaceGameArea) ServiceLocator.getGameArea();
+        SpaceGameArea spaceGameArea = (SpaceGameArea) gameArea;
+        EntitiesSpawner gameAreaHostileSpawner = spaceGameArea.getHostileSpawner();
+        verify(gameAreaHostileSpawner).startPeriodicSpawning();    }
 }
