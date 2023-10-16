753a06c6282478e8c7d83acd7ff2e49cc8cdb236
cmp6 <s4650089@student.uq.edu.au>
2023-10-16 21:52:40 +1000

    remove unused import

--- source/core/src/test/com/csse3200/game/components/placeables/SprinklerComponentTest.java
+++ source/core/src/test/com/csse3200/game/components/placeables/SprinklerComponentTest.java
@@ -15,7 +15,6 @@ import com.csse3200.game.services.ServiceLocator;
 import com.csse3200.game.services.TimeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.*;
